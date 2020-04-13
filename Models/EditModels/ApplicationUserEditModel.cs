32f90a296f12fad228dd4d9dd5437ca6208f19fc
Carlos Pavajeau <cantte098@gmail.com>
2020-04-13 15:29:19 -0500

    BackEnd/EditModels: Add ApplicationUserEditModel class

---
+++ Models/EditModels/ApplicationUserEditModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kaizen.Models.EditModels
+{
+    public class ApplicationUserEditModel
+    {
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
