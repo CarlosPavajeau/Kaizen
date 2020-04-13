bd4cfdb84e6ebbbfea7a30b20109fc625327e281
Carlos Pavajeau <cantte098@gmail.com>
2020-04-13 15:29:41 -0500

    BackEnd/EditModels: Add ApplicationUserInputModel class

---
+++ Models/InputModels/ApplicationUserInputModel.cs
@@ -0,0 +1,13 @@
+﻿using Kaizen.Models.EditModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kaizen.Models.InputModels
+{
+    public class ApplicationUserInputModel : ApplicationUserEditModel
+    {
+        public string Username { get; set; }
+    }
+}
