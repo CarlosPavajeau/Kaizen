6cbe5b31621a0d13c2f96daedc73454cf5e95639
Carlos Pavajeau <cantte098@gmail.com>
2020-04-13 15:28:46 -0500

    BackEnd/Data: Add ApplicationDbContext class

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Kaizen.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kaizen.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
