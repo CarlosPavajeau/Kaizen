787a3d4d1ad5b988a98423e676d18ae183164bfd
Carlos Pavajeau <cantte098@gmail.com>
2020-04-13 15:31:20 -0500

    BackEnd/InputModels: Add LoginRequest class

---
+++ Models/InputModels/LoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Kaizen.Models.InputModels
+{
+    public class LoginRequest
+    {
+        public string UsernameOrEmail { get; set; }
+        public string Password { get; set; }
+    }
+}
