36f822779a8a83922216e68f68099dc5ddd58f7e
Carlos Pavajeau <cantte098@gmail.com>
2020-04-13 15:30:03 -0500

    BackEnd/ViewModels: Add ApplicationUserViewModel class

---
+++ Models/ViewModels/ApplicationUserViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Kaizen.Models.InputModels;
+
+namespace Kaizen.Models.ViewModels
+{
+    public class ApplicationUserViewModel : ApplicationUserInputModel
+    {
+        public ApplicationUserViewModel(ApplicationUser user)
+        {
+            Id = user.Id;
+            Username = user.UserName;
+            Email = user.Email;
+            PhoneNumber = user.PhoneNumber;
+            Password = null;
+        }
+
+        public string Token { get; set; }
+        public string Id { get; set; }
+    }
+}
