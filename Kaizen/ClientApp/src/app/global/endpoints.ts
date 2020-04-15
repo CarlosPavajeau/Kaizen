9391240f8cfa9c88cc26f57905a487d3055d2c2f
Carlos Pavajeau <cantte098@gmail.com>
2020-04-15 17:14:16 -0500

    FrontEnd/Global: Add endpoints and global imports

---
+++ Kaizen/ClientApp/src/app/global/endpoints.ts
@@ -0,0 +1,6 @@
+export class Endpoints {
+  public static get AuthUrl() : string {
+    return "api/Users";
+  }
+
+}
--- Kaizen/ClientApp/src/app/global/global.module.ts
+++ Kaizen/ClientApp/src/app/global/global.module.ts
@@ -1,12 +1,23 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { MaterialModule } from '../core/material.module';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    CommonModule,
+    HttpClientModule
   ]
 })
-export class GlobalModule { }
+export class GlobalModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: GlobalModule
+    };
+  }
+}
