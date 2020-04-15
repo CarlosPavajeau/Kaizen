1af5f2a52de781569fd2803fec697b6304daeb8c
Carlos Pavajeau <cantte098@gmail.com>
2020-04-15 12:12:32 -0500

    FrontEnd/Core: Add api-prefix interceptor

---
+++ Kaizen/ClientApp/src/app/core/http/api-prefix.interceptor.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class ApiPrefixInterceptor implements HttpInterceptor {
+
+  constructor() {}
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    request = request.clone({
+      setHeaders: {
+        'Content-Type': 'application/json'
+      }
+    });
+    return next.handle(request);
+  }
+}
