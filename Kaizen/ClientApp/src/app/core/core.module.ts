14c0dd83bd06d2e8069a2ef68caab58785318df1
Carlos Pavajeau <cantte098@gmail.com>
2020-04-15 17:13:27 -0500

    FrontEnd/Core: Add material module imports and core providers

--- Kaizen/ClientApp/src/app/core/core.module.ts
+++ Kaizen/ClientApp/src/app/core/core.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { HttpClientModule } from '@angular/common/http';
+import { ApiPrefixInterceptor } from './http/api-prefix.interceptor';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule,
+  ],
+  providers: [
+    ApiPrefixInterceptor
   ]
 })
 export class CoreModule { }
--- Kaizen/ClientApp/src/app/core/material.module.ts
+++ Kaizen/ClientApp/src/app/core/material.module.ts
@@ -1,12 +1,45 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { MatButtonModule } from "@angular/material/button";
+import { MatInputModule } from "@angular/material/input";
+import { MatRadioModule } from "@angular/material/radio";
+import { MatCardModule } from "@angular/material/card";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatIconModule } from "@angular/material/icon";
+import { MatExpansionModule } from "@angular/material/expansion";
+import { MatTableModule } from "@angular/material/table";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatChipsModule } from '@angular/material/chips';
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatTooltipModule } from "@angular/material/tooltip";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatListModule } from "@angular/material/list";
 
-
+const MaterialModules = [
+  MatButtonModule,
+  MatInputModule,
+  MatRadioModule,
+  MatCardModule,
+  MatToolbarModule,
+  MatMenuModule,
+  MatIconModule,
+  MatExpansionModule,
+  MatTableModule,
+  MatGridListModule,
+  MatDialogModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatPaginatorModule,
+  MatTooltipModule,
+  MatSidenavModule,
+  MatListModule
+];
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [MaterialModules],
+  exports: [MaterialModules]
 })
 export class MaterialModule { }
