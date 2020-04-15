a6f270fe5eb5728a4fa2e153612cf688a724e876
Carlos Pavajeau <cantte098@gmail.com>
2020-04-15 17:17:55 -0500

    FrontEnd/Shared: Add shared imports and exports

--- Kaizen/ClientApp/src/app/shared/shared.module.ts
+++ Kaizen/ClientApp/src/app/shared/shared.module.ts
@@ -1,13 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Page404Component } from './components/page404/page404.component';
-
-
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { MaterialModule } from '../core/material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [Page404Component],
+  declarations: [
+    Page404Component,
+    HeaderComponent,
+    FooterComponent,
+    NavMenuComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule,
+    BrowserAnimationsModule
+  ],
+  exports: [
+    Page404Component,
+    HeaderComponent,
+    FooterComponent,
+    NavMenuComponent,
+    MaterialModule,
+    CommonModule,
+    BrowserAnimationsModule
   ]
 })
 export class SharedModule { }
