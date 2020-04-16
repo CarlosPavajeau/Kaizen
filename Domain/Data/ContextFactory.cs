4515f21d055d05776530187b91a1941213d23fc6
Carlos Pavajeau <cantte098@gmail.com>
2020-04-15 20:28:20 -0500

    BackEnd/Domain/Data: Add ContextFactory class

---
+++ Domain/Data/ContextFactory.cs
@@ -0,0 +1,32 @@
+﻿using Kaizen.Core.DependencyResolver;
+using Kaizen.Domain.Data.Configuration;
+using Microsoft.Extensions.Options;
+using System;
+using System.Linq;
+
+namespace Kaizen.Domain.Data
+{
+    public class ContextFactory : IContextFactory
+    {
+        private Configuration.Data DataConfuguration { get; }
+        private ConnectionStrings ConnectionStrings { get; }
+        private readonly IResolver _resolver;
+
+        public ContextFactory(IOptions<Configuration.Data> dataOptions, IResolver resolver, IOptions<ConnectionStrings> connectionStringOptions)
+        {
+            DataConfuguration = dataOptions.Value;
+            ConnectionStrings = connectionStringOptions.Value;
+            _resolver = resolver;
+        }
+        public ApplicationDbContext Create()
+        {
+            var dataProvider = _resolver.ResolveAll<IDataProvider>()
+                .SingleOrDefault(x => x.Provider == DataConfuguration.Provider);
+
+            if (dataProvider == null)
+                throw new Exception("The Data Provider entry in appsettings.json is empty or the one specified has not been found!");
+
+            return dataProvider.CreateDbContext(ConnectionStrings.DefaultConnection);
+        }
+    }
+}
