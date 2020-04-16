2c46146bdeddb8fba1f7e599cc53ec2bd4dbf5a6
Carlos Pavajeau <cantte098@gmail.com>
2020-04-15 20:27:44 -0500

    BackEnd/Domain/Data: Add IDataProvider interface

---
+++ Domain/Data/IDataProvider.cs
@@ -0,0 +1,13 @@
+﻿using Kaizen.Domain.Data.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Kaizen.Domain.Data
+{
+    public interface IDataProvider
+    {
+        DataProvider Provider { get; }
+
+        IServiceCollection RegisterDbContext(IServiceCollection services, string connectionString);
+        ApplicationDbContext CreateDbContext(string connectionString);
+    }
+}
