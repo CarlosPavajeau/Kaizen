1710fe877963f1c46f60a1f4634f829a5cb90185
Carlos Pavajeau <cantte098@gmail.com>
2020-04-17 12:16:39 -0500

    FrontEnd/App: Add new routes

--- Kaizen/ClientApp/src/app/app-routing.module.ts
+++ Kaizen/ClientApp/src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ import { Page404Component } from './shared/components/page404/page404.component'
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'user', loadChildren: () => import('./users/user.module').then(m => m.UserModule) },
+  { path: 'user', loadChildren: () => import('./modules/users/user.module').then(m => m.UserModule) },
+  { path: 'client', loadChildren: () => import('./modules/clients/client.module').then(m => m.ClientModule) },
 
   { path: '**', component: Page404Component }
 ];
