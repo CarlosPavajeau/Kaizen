c95c12e7e9232d520b62a2a81d4cb0010778bbf8
Carlos Pavajeau <cantte098@gmail.com>
2020-04-16 20:24:16 -0500

    FrontEnd/User/Login: Some improvements

--- Kaizen/ClientApp/src/app/users/components/user-login/user-login.component.ts
+++ Kaizen/ClientApp/src/app/users/components/user-login/user-login.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { User } from '../../models/user';
 import { AuthService } from '../../services/auth.service';
 import { LoginRequest } from '../../models/login-request';
 
