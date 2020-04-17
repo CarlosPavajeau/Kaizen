ba81f86857867ae2e8d8dd6a955b5d7a81c25dbd
Carlos Pavajeau <cantte098@gmail.com>
2020-04-16 20:31:55 -0500

    FrontEnd/App: Remove unnecessary imports

--- Kaizen/ClientApp/src/app/app.module.ts
+++ Kaizen/ClientApp/src/app/app.module.ts
@@ -1,21 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './shared/nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './core/material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
 import { GlobalModule } from './global/global.module';
 import { SharedModule } from './shared/shared.module';
-import { UserModule } from './users/user.module';
 
 @NgModule({
   declarations: [
@@ -28,8 +21,7 @@ import { UserModule } from './users/user.module';
     GlobalModule.forRoot(),
     AppRoutingModule,
     CoreModule,
-    SharedModule,
-    UserModule
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
