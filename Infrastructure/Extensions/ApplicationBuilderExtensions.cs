450be0979151a41f16075cc0122ba2a5fabf3b7f
Carlos Pavajeau <cantte098@gmail.com>
2020-04-16 20:14:08 -0500

    BackEnd/Infrastructure: Add ApplicationBuilder and ServiceCollection extensions for use Swagger and Jwt

---
+++ Infrastructure/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Kaizen.Infrastructure.Extensions
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder UseSwaggerApiDocumentation(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(s =>
+            {
+                s.SwaggerEndpoint("/swagger/v1/swagger.json", "My API version-1");
+            });
+
+            return app;
+        }
+    }
+}
---
+++ Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Text;
+
+namespace Kaizen.Infrastructure.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(s =>
+            {
+                s.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Ecolplag API",
+                    Description = "Ecolplag API - ASP.NET Core Web",
+                    TermsOfService = new Uri("https://cla.dotnetfoundation.org/"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Carlos Andrés Pavajeau Max",
+                        Email = "cantte098@gmial.com",
+                        Url = new Uri("https://github.com/cantte/")
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Dotnet foundation license",
+                        Url = new Uri("https://www.byasystems.co/license")
+                    }
+                });
+            });
+
+            return services;
+        }
+
+        public static IServiceCollection AddJwtAuthentication(this IServiceCollection services, IConfiguration configuration)
+        {
+            byte[] key = Encoding.ASCII.GetBytes(configuration.GetSection("AppSettings")["Key"]);
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).
+            AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+            return services;
+        }
+    }
+}
