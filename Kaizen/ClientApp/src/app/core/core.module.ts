92aa1c6c42cf4543971b515114073c40e3a3e97c
Carlos Pavajeau <cantte098@gmail.com>
2020-04-16 20:18:02 -0500

    FrontEnd/Core: Add new core modules

--- Kaizen/ClientApp/src/app/core/core.module.ts
+++ Kaizen/ClientApp/src/app/core/core.module.ts
@@ -2,16 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { ApiPrefixInterceptor } from './http/api-prefix.interceptor';
-import { MaterialModule } from './material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
   declarations: [],
   imports: [
+    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     CommonModule,
     HttpClientModule,
+    BrowserAnimationsModule
   ],
-  providers: [
-    ApiPrefixInterceptor
-  ]
+  providers: [ApiPrefixInterceptor]
 })
 export class CoreModule { }
