d33349eb8cc060e6da27b37c86f519081b1891be
Carlos Pavajeau <cantte098@gmail.com>
2020-04-16 20:23:40 -0500

    FrontEnd/User: Rename file and and lazy loading

--- Kaizen/ClientApp/src/app/users/user-routing.module.ts
+++ Kaizen/ClientApp/src/app/users/user-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes, Router } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { UserLoginComponent } from './components/user-login/user-login.component';
 import { UserRegisterComponent } from './components/user-register/user-register.component';
 import { UserProfileComponent } from './components/user-profile/user-profile.component';
 
 const routes: Routes = [
-  { path: 'user/login', component: UserLoginComponent },
-  { path: 'user/register', component: UserRegisterComponent },
-  { path: 'user/profile', component: UserProfileComponent }
+  { path: '', children: [
+    { path: 'login', component: UserLoginComponent},
+    { path: 'register', component: UserRegisterComponent },
+    { path: 'profile', component: UserProfileComponent },
+    { path: '', redirectTo: 'profile', pathMatch: 'full'}
+  ] }
 ];
 
 @NgModule({
