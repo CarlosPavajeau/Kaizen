ae0c848f5ec3733ccbf36cf94bdf34d567b4910c
Carlos Pavajeau <cantte098@gmail.com>
2020-04-17 12:07:36 -0500

    FrontEnd/Core: Add authentication service

---
+++ Kaizen/ClientApp/src/app/core/authentication/authentication.service.ts
@@ -0,0 +1,70 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../models/user';
+import { Observable } from 'rxjs';
+import { Endpoints } from 'src/app/global/endpoints';
+import { tap } from 'rxjs/operators';
+import { LoginRequest } from '../models/login-request';
+import { isNullOrUndefined } from 'util';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+
+  readonly USER_LOCALSTORAGE_KEY = 'currentUser';
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  registerUser(user: User): Observable<User> {
+    return this.http.post<User>(Endpoints.AuthUrl, user)
+      .pipe(
+        tap(_ => console.log('Msg'))
+      )
+  }
+
+  loginUser(user: LoginRequest): Observable<User> {
+    return this.http.post<User>(`${Endpoints.AuthUrl}/Login`, user)
+      .pipe(
+        tap(_ => console.log('Msg'))
+      )
+  }
+
+  logoutUser(): void {
+    this.removeUser();
+  }
+
+  removeUser(): void {
+    localStorage.removeItem(this.USER_LOCALSTORAGE_KEY);
+  }
+
+  setCurrentUser(user: User): void {
+    let user_str = JSON.stringify(user);
+    localStorage.setItem(this.USER_LOCALSTORAGE_KEY, user_str);
+  }
+
+  getToken(): string {
+    const user: User = this.getCurrentUser();
+    if (user) {
+      return user.token;
+    } else {
+      return '';
+    }
+  }
+
+  getCurrentUser(): User {
+    let user_str = localStorage.getItem(this.USER_LOCALSTORAGE_KEY);
+    if (isNullOrUndefined(user_str)) {
+      return null;
+    } else {
+      const user: User = JSON.parse(user_str);
+      return user;
+    }
+  }
+
+  userLoggedIn(): boolean {
+    return this.getCurrentUser() != null;
+  }
+}
