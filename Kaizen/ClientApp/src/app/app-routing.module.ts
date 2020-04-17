a4fc5917855da71889ee2b3db3a37f3e6c931d02
Carlos Pavajeau <cantte098@gmail.com>
2020-04-16 20:27:03 -0500

    FrontEnd/App-Routing: Add new routes and add Lazi Loading feature

--- Kaizen/ClientApp/src/app/app-routing.module.ts
+++ Kaizen/ClientApp/src/app/app-routing.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from "@angular/router";
 import { HomeComponent } from './home/home.component';
+import { Page404Component } from './shared/components/page404/page404.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'user', loadChildren: () => import('./users/user.module').then(m => m.UserModule) },
+
+  { path: '**', component: Page404Component }
 ];
 
 @NgModule({
   declarations: [],
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { enableTracing: false })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
