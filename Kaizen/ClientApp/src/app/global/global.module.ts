6f7429b7e64fa6ca547535926195fc1e5b4b30df
Carlos Pavajeau <cantte098@gmail.com>
2020-04-16 20:19:27 -0500

    FrontEnd/Global: Remove unnecessary imports

--- Kaizen/ClientApp/src/app/global/global.module.ts
+++ Kaizen/ClientApp/src/app/global/global.module.ts
@@ -1,15 +1,10 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { BrowserModule } from '@angular/platform-browser';
-import { MaterialModule } from '../core/material.module';
-
-
 
 @NgModule({
   declarations: [],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     CommonModule,
     HttpClientModule
   ]
