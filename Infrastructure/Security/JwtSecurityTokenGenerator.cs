6ca3911f5d6d9fae88d8accc78f20cfafef7f95e
Carlos Pavajeau <cantte098@gmail.com>
2020-04-18 16:52:49 -0500

    BackEnd/Infrastructure: Add JwtSecurityTokenGenerator class

---
+++ Infrastructure/Security/JwtSecurityTokenGenerator.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Kaizen.Infrastructure.Security
+{
+    public static class JwtSecurityTokenGenerator
+    {
+        public static string GenerateSecurityToken(IConfiguration configuration, string username)
+        {
+            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+            byte[] key = Encoding.ASCII.GetBytes(configuration.GetSection("AppSettings")["Key"]);
+
+            SecurityTokenDescriptor tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, username)
+                }),
+                Expires = DateTime.Now.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            SecurityToken token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
