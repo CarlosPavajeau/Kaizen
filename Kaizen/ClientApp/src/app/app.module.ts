8f320d94483cd362cedb5ae1338f0efcc03d3b9f
Carlos Pavajeau <cantte098@gmail.com>
2020-04-17 20:45:56 -0500

    FrontEnd: Remove unnecessary components

--- Kaizen/ClientApp/src/app/app.module.ts
+++ Kaizen/ClientApp/src/app/app.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
 import { GlobalModule } from './global/global.module';
@@ -13,9 +10,7 @@ import { SharedModule } from './shared/shared.module';
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    CounterComponent,
-    FetchDataComponent
+    HomeComponent
   ],
   imports: [
     CoreModule.forRoot(),
