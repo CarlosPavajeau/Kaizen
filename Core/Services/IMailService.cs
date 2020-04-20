2d8fd7a6170485afe1e15e43987b628312e27d9f
Carlos Pavajeau <cantte098@gmail.com>
2020-04-19 19:33:52 -0500

    BackEnd/Core: Add ISmsService and IMailService

---
+++ Core/Services/IMailService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Kaizen.Core.Services
+{
+    public interface IMailService
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
---
+++ Core/Services/ISmsService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Kaizen.Core.Services
+{
+    public interface ISmsService
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
