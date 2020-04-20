ea24482ce763fe3760463f7a21d21ff093e9d710
Carlos Pavajeau <cantte098@gmail.com>
2020-04-19 19:35:12 -0500

    BackEnd/Infrastructure: Add MailService and SmsService

---
+++ Infrastructure/Services/MailService.cs
@@ -0,0 +1,14 @@
+﻿using Kaizen.Core.Services;
+using System;
+using System.Threading.Tasks;
+
+namespace Kaizen.Infrastructure.Services
+{
+    public class MailService : IMailService
+    {
+        public Task SendEmailAsync(string email, string subject, string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Infrastructure/Services/SmsService.cs
@@ -0,0 +1,14 @@
+﻿using Kaizen.Core.Services;
+using System;
+using System.Threading.Tasks;
+
+namespace Kaizen.Infrastructure.Services
+{
+    public class SmsService : ISmsService
+    {
+        public Task SendSmsAsync(string number, string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
