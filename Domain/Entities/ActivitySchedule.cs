e6f5d090584f4aa88f93f4830b9ad3f633c40172
Carlos Pavajeau <cantte098@gmail.com>
2020-04-21 18:56:05 -0500

    BackEnd/Entities/ActivitySchedule: Add 'Year' and 'Activities' attributes

--- Domain/Entities/ActivitySchedule.cs
+++ Domain/Entities/ActivitySchedule.cs
@@ -1,10 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Kaizen.Domain.Entities
 {
     public class ActivitySchedule
     {
+        [Key]
+        public int Year { get; set; }
+
+        public List<Activity> Activities { get; set; }
     }
 }
