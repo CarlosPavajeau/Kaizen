d89b6236bb51a9348ac9fe8b48b831fba5ab3671
Carlos Pavajeau <cantte098@gmail.com>
2020-04-21 18:54:54 -0500

    BackEnd/Entities/SafetySheet: Add Key and MaxLength Attributes

--- Domain/Entities/SafetySheet.cs
+++ Domain/Entities/SafetySheet.cs
@@ -1,7 +1,13 @@
-﻿namespace Kaizen.Domain.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Kaizen.Domain.Entities
 {
     public class SafetySheet
     {
+        [Key, MaxLength(15)]
         public string ProductCode { get; set; }
+        [ForeignKey("ProductCode")]
+        public Product Product { get; set; }
     }
 }
