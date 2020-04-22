a2664c9d3048ab40f55800e71de321f8535f9f48
Carlos Pavajeau <cantte098@gmail.com>
2020-04-21 19:46:51 -0500

    BackEnd/Controllers: Add Clients controller

---
+++ Kaizen/Controllers/ClientsController.cs
@@ -0,0 +1,125 @@
+﻿using Kaizen.Domain.Data;
+using Kaizen.Domain.Entities;
+using Kaizen.EditModels;
+using Kaizen.InputModels;
+using Kaizen.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kaizen.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ClientsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Clients
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ClientViewModel>>> GetClients()
+        {
+            return await _context.Clients.Select(c => new ClientViewModel(c)).ToListAsync();
+        }
+
+        // GET: api/Clients/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ClientViewModel>> GetClient(string id)
+        {
+            Client client = await _context.Clients.FindAsync(id);
+
+            if (client == null)
+            {
+                return NotFound();
+            }
+
+            return new ClientViewModel(client);
+        }
+
+        // PUT: api/Clients/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutClient(string id, ClientEditModel client)
+        {
+            //if (id != client.Id)
+            //{
+            //    return BadRequest();
+            //}
+
+            _context.Entry(client).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ClientExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Clients
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<ClientViewModel>> PostClient(ClientInputModel client)
+        {
+            //_context.Clients.Add(client);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (ClientExists(client.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetClient", new { id = client.Id }, client);
+        }
+
+        // DELETE: api/Clients/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<ClientViewModel>> DeleteClient(string id)
+        {
+            Client client = await _context.Clients.FindAsync(id);
+            if (client == null)
+            {
+                return NotFound();
+            }
+
+            _context.Clients.Remove(client);
+            await _context.SaveChangesAsync();
+
+            return new ClientViewModel(client);
+        }
+
+        private bool ClientExists(string id)
+        {
+            return _context.Clients.Any(e => e.Id == id);
+        }
+    }
+}
