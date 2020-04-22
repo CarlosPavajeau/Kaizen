d4151e7491f8dea3babae38bcd83fbd1598e76c4
Carlos Pavajeau <cantte098@gmail.com>
2020-04-21 19:27:30 -0500

    BackEnd/Entities: Make Invoice attributes

--- Domain/Entities/Invoice.cs
+++ Domain/Entities/Invoice.cs
@@ -1,10 +1,25 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Kaizen.Domain.Entities
 {
+    public enum InvoiceState
+    {
+        Generated,
+        Regenerated,
+        Paid,
+        Expired
+    }
     public class Invoice
     {
         [Key]
         public int Id { get; set; }
+        public InvoiceState State { get; set; }
+        public decimal IVA { get; set; }
+        public decimal SubTotal { get; set; }
+        public decimal Total { get; set; }
+
+        [ForeignKey("ClientId")]
+        public Client Client { get; set; }
     }
 }
