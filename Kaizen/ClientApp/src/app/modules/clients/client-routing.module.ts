75d8b66a3030cc458431ecc0dd526dccd2d10b19
Carlos Pavajeau <cantte098@gmail.com>
2020-04-21 21:13:41 -0500

    FrontEnd/Clients: Add new routes

--- Kaizen/ClientApp/src/app/modules/clients/client-routing.module.ts
+++ Kaizen/ClientApp/src/app/modules/clients/client-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ClientRegisterComponent } from './components/client-register/client-register.component';
+import { ClientsComponent } from './components/clients/clients.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', children: [
+    { path: 'register', component: ClientRegisterComponent },
+    { path: 'clients', component: ClientsComponent },
+    { path: '', redirectTo: '/', pathMatch: 'full' }
+  ] }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
