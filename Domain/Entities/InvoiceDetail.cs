9c805cb3aafbed7130ec98c61b7dd2df4547a182
Carlos Pavajeau <cantte098@gmail.com>
2020-04-21 19:26:40 -0500

    BackEnd/Entities: Add 'IInvoiceDetail' interface

---
+++ Domain/Entities/InvoiceDetail.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kaizen.Domain.Entities
+{
+    public interface IInvoiceDetail<T>
+    {
+        public T Detail { get; set; }
+    }
+}
