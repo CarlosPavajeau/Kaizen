1c472db2c755ee9f18d3ef2031c5f6247e61db30
Carlos Pavajeau <cantte098@gmail.com>
2020-04-23 09:28:24 -0500

    FrontEnd/Core: Change double quotes to simple quotes

--- Kaizen/ClientApp/src/app/core/material.module.ts
+++ Kaizen/ClientApp/src/app/core/material.module.ts
@@ -1,22 +1,24 @@
 import { NgModule } from '@angular/core';
-import { MatButtonModule } from "@angular/material/button";
-import { MatInputModule } from "@angular/material/input";
-import { MatRadioModule } from "@angular/material/radio";
-import { MatCardModule } from "@angular/material/card";
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { MatMenuModule } from "@angular/material/menu";
-import { MatIconModule } from "@angular/material/icon";
-import { MatExpansionModule } from "@angular/material/expansion";
-import { MatTableModule } from "@angular/material/table";
-import { MatGridListModule } from "@angular/material/grid-list";
-import { MatDialogModule } from "@angular/material/dialog";
-import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatTableModule } from '@angular/material/table';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatChipsModule } from '@angular/material/chips';
-import { MatPaginatorModule } from "@angular/material/paginator";
-import { MatTooltipModule } from "@angular/material/tooltip";
-import { MatSidenavModule } from "@angular/material/sidenav";
-import { MatListModule } from "@angular/material/list";
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSelectModule } from '@angular/material/select';
+import { MatStepperModule } from '@angular/material/stepper';
 
 const MaterialModules = [
   MatButtonModule,
@@ -36,7 +38,9 @@ const MaterialModules = [
   MatTooltipModule,
   MatSidenavModule,
   MatListModule,
-  MatProgressSpinnerModule
+  MatProgressSpinnerModule,
+  MatSelectModule,
+  MatStepperModule
 ];
 
 @NgModule({
