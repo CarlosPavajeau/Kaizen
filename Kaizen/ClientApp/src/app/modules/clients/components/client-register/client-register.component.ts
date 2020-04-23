13b8a28b6b0720b823eb71c49df03d8848a740c3
Carlos Pavajeau <cantte098@gmail.com>
2020-04-23 09:37:19 -0500

    FrontEnd/Clients: Make a new incomplete client register form #2

--- Kaizen/ClientApp/src/app/modules/clients/components/client-register/client-register.component.ts
+++ Kaizen/ClientApp/src/app/modules/clients/components/client-register/client-register.component.ts
@@ -3,6 +3,9 @@ import { ClientService } from '../../services/client.service';
 import { FormBuilder, FormGroup, AbstractControl, Validators } from '@angular/forms';
 import { IForm } from 'src/app/core/models/form';
 import { CharactersValidators } from 'src/app/shared/validators/characters-validators';
+import { User } from 'src/app/core/models/user';
+import { AuthenticationService } from 'src/app/core/authentication/authentication.service';
+import { ClientExistsValidator } from 'src/app/shared/validators/client-exists-validator';
 
 @Component({
   selector: 'app-client-register',
@@ -12,6 +15,9 @@ import { CharactersValidators } from 'src/app/shared/validators/characters-valid
 export class ClientRegisterComponent implements OnInit, IForm {
 
   clientForm: FormGroup;
+  legalPersonForm: FormGroup;
+  contactPersonForm: FormGroup;
+  user: User;
 
   public get controls() : { [key: string]: AbstractControl; } {
     return this.clientForm.controls;
@@ -19,7 +25,9 @@ export class ClientRegisterComponent implements OnInit, IForm {
 
   constructor(
     private clientService: ClientService,
-    private formBuilder: FormBuilder
+    private authService: AuthenticationService,
+    private formBuilder: FormBuilder,
+    private clientValidator: ClientExistsValidator
   ) { }
 
   ngOnInit(): void {
@@ -28,16 +36,26 @@ export class ClientRegisterComponent implements OnInit, IForm {
 
   initForm() {
     this.clientForm = this.formBuilder.group({
-      id: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(10), CharactersValidators.numericCharacters]],
+      id: ['', {
+        validators: [Validators.required, Validators.minLength(8), Validators.maxLength(10), CharactersValidators.numericCharacters],
+        asyncValidators: [this.clientValidator.validate.bind(this.clientValidator)],
+        updateOn: 'blur'
+      }],
       firstName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(20), CharactersValidators.alphabeticCharacters]],
       secondName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(20), CharactersValidators.alphabeticCharacters]],
       lastName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(20), CharactersValidators.alphabeticCharacters]],
-      secondLastname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(20), CharactersValidators.alphabeticCharacters]]
+      secondLastname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(20), CharactersValidators.alphabeticCharacters]],
+      clientType: ['', [Validators.required]]
+    });
+
+    this.legalPersonForm = this.formBuilder.group({
+      tradeName: ['', [Validators.required]],
+      businessName: ['', [Validators.required]]
     });
   }
 
-  onClick() {
-    console.log(this.controls['id'].errors);
-  }
+  onSubmit(user: User)
+  {
 
+  }
 }
