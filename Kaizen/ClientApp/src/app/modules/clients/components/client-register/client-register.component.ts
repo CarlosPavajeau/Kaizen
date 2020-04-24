3c1f701a450b35d7e2bea6d1ea31f3ce5c2e7a89
Carlos Pavajeau <cantte098@gmail.com>
2020-04-23 20:41:53 -0500

    FrontEnd/Clients: Make an almost complete registration form

--- Kaizen/ClientApp/src/app/modules/clients/components/client-register/client-register.component.ts
+++ Kaizen/ClientApp/src/app/modules/clients/components/client-register/client-register.component.ts
@@ -6,6 +6,7 @@ import { CharactersValidators } from 'src/app/shared/validators/characters-valid
 import { User } from 'src/app/core/models/user';
 import { AuthenticationService } from 'src/app/core/authentication/authentication.service';
 import { ClientExistsValidator } from 'src/app/shared/validators/client-exists-validator';
+import { Client } from '../../models/client';
 
 @Component({
   selector: 'app-client-register',
@@ -17,10 +18,24 @@ export class ClientRegisterComponent implements OnInit, IForm {
   clientForm: FormGroup;
   legalPersonForm: FormGroup;
   contactPersonForm: FormGroup;
+  ubicationForm: FormGroup;
+
   user: User;
 
   public get controls() : { [key: string]: AbstractControl; } {
     return this.clientForm.controls;
+  }
+
+  public get legal_controls(): { [key: string]: AbstractControl; } {
+    return this.legalPersonForm.controls;
+  }
+
+  public get contact_controls(): { [key: string]: AbstractControl; } {
+    return this.contactPersonForm.controls;
+  }
+
+  public get ubication_controls(): { [key: string]: AbstractControl; } {
+    return this.ubicationForm.controls;
   }
 
   constructor(
@@ -35,6 +50,39 @@ export class ClientRegisterComponent implements OnInit, IForm {
   }
 
   initForm() {
+    this.initClientForm();
+    this.initLegalPersonForm();
+    this.initContactForm();
+    this.initUbicationForm();
+  }
+
+  private initUbicationForm() {
+    this.ubicationForm = this.formBuilder.group({
+      city: ['', [Validators.required]],
+      neighborhood: ['', [Validators.required]],
+      street: ['', Validators.required]
+    });
+  }
+
+  private initContactForm() {
+    this.contactPersonForm = this.formBuilder.group({
+      firstPhonenumber: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(10), CharactersValidators.numericCharacters]],
+      secondPhonenumber: ['', [Validators.minLength(10), Validators.maxLength(10), CharactersValidators.numericCharacters]],
+      firstLandline: ['', [Validators.minLength(10), Validators.maxLength(15), CharactersValidators.numericCharacters]],
+      secondLandline: ['', [Validators.minLength(10), Validators.maxLength(15), CharactersValidators.numericCharacters]],
+      email: ['', [Validators.required, Validators.email]]
+    });
+  }
+
+  private initLegalPersonForm() {
+    this.legalPersonForm = this.formBuilder.group({
+      NIT: ['', Validators.required],
+      tradeName: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(50)]],
+      businessName: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(50)]]
+    });
+  }
+
+  private initClientForm() {
     this.clientForm = this.formBuilder.group({
       id: ['', {
         validators: [Validators.required, Validators.minLength(8), Validators.maxLength(10), CharactersValidators.numericCharacters],
@@ -47,15 +95,48 @@ export class ClientRegisterComponent implements OnInit, IForm {
       secondLastname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(20), CharactersValidators.alphabeticCharacters]],
       clientType: ['', [Validators.required]]
     });
-
-    this.legalPersonForm = this.formBuilder.group({
-      tradeName: ['', [Validators.required]],
-      businessName: ['', [Validators.required]]
-    });
   }
 
   onSubmit(user: User)
   {
+    if (user) {
+      if (this.allFormsValid()) {
+        let client: Client = {
+          id: this.controls['id'].value,
+          firstName: this.controls['firstName'].value,
+          secondName: this.controls['secondName'].value,
+          lastName: this.controls['lastName'].value,
+          secondLastName: this.controls['secondLastname'].value,
+          clientType: this.controls['clientType'].value,
+          firstPhoneNumber: this.contact_controls['firstPhonenumber'].value,
+          secondPhoneNumber: this.contact_controls['secondPhonenumber'].value,
+          firstLandline: this.contact_controls['firstLandline'].value,
+          secondLandline: this.contact_controls['secondLandline'].value,
+          contactPeople: [
 
+          ],
+          clientAddress: {
+            city: this.ubication_controls['city'].value,
+            neighborhood: this.ubication_controls['neighborhood'].value,
+            street: this.ubication_controls['street'].value
+          }
+        };
+
+        if (client.clientType == 'JuridicPerson') {
+          client.NIT = this.legal_controls['NIT'].value;
+          client.busninessName = this.legal_controls['businessName'].value;
+          client.tradeName = this.legal_controls['tradeName'].value;
+        }
+
+        console.log(client);
+        console.log(user);
+      }
+    }
+  }
+
+  allFormsValid() : boolean {
+    return this.clientForm.valid && this.contactPersonForm.valid &&
+      this.ubicationForm.valid &&
+      ((this.controls['clientType'].value == 'JuridicPerson') ? this.legalPersonForm.valid : true);
   }
 }
