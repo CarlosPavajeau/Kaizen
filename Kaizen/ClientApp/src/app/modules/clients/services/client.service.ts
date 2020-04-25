ef86b4ebda27e0eaf4bb09747aae4cc1f484293d
Carlos Pavajeau <cantte098@gmail.com>
2020-04-25 12:01:24 -0500

    FrontEnd/Clients: Remove unnecessary pipes

--- Kaizen/ClientApp/src/app/modules/clients/services/client.service.ts
+++ Kaizen/ClientApp/src/app/modules/clients/services/client.service.ts
@@ -3,7 +3,6 @@ import { HttpClient } from '@angular/common/http';
 import { Client } from '../models/client';
 import { Observable } from 'rxjs';
 import { Endpoints } from 'src/app/global/endpoints';
-import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -15,30 +14,18 @@ export class ClientService {
   ) { }
 
   getClients(): Observable<Client[]> {
-    return this.http.get<Client[]>(Endpoints.ClientsUrl)
-      .pipe(
-        tap(_ => console.log('Hola'))
-      );
+    return this.http.get<Client[]>(Endpoints.ClientsUrl);
   }
 
   getClient(id: string): Observable<Client> {
-    return this.http.get<Client>(`${Endpoints.ClientsUrl}/${id}`)
-      .pipe(
-        tap(_ => console.log('Test'))
-      );
+    return this.http.get<Client>(`${Endpoints.ClientsUrl}/${id}`);
   }
 
   saveClient(client: Client): Observable<Client> {
-    return this.http.post<Client>(Endpoints.ClientsUrl, client)
-      .pipe(
-        tap(_ => console.log('Hola'))
-      );
+    return this.http.post<Client>(Endpoints.ClientsUrl, client);
   }
 
   updateClient(client: Client): Observable<Client> {
-    return this.http.put<Client>(`${Endpoints.ClientsUrl}/${client.id}`, client)
-      .pipe(
-        tap(_ => console.log('Hola'))
-      );
+    return this.http.put<Client>(`${Endpoints.ClientsUrl}/${client.id}`, client);
   }
 }
