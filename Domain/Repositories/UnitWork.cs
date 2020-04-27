f4019ae574444ca5a72e00a85b628addc0708597
Carlos Pavajeau <cantte098@gmail.com>
2020-04-27 14:40:09 -0500

    BackEnd/Domain: Add UnitWork class

---
+++ Domain/Repositories/UnitWork.cs
@@ -0,0 +1,34 @@
+﻿using Kaizen.Domain.Data;
+using System.Threading.Tasks;
+
+namespace Kaizen.Domain.Repositories
+{
+    public class UnitWork : IUnitWork
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public UnitWork(
+            ApplicationDbContext applicationDbContext,
+            IApplicationUserRepository userRepository,
+            IClientsRepository clientsRepository
+            )
+        {
+            _dbContext = applicationDbContext;
+            ApplicationUsers = userRepository;
+            Clients = clientsRepository;
+        }
+        public IClientsRepository Clients { get; }
+
+        public IApplicationUserRepository ApplicationUsers { get; }
+
+        public void Dispose()
+        {
+            _dbContext.Dispose();
+        }
+
+        public async Task Save()
+        {
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
