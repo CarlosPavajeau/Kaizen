cedfa158350a4154aa694eab909bc47600d7fdf3
Carlos Pavajeau <cantte098@gmail.com>
2020-04-27 14:38:17 -0500

    BackEnd/Domain: Add IApplicationUserRepository and IClientsRepository interfaces

---
+++ Domain/Repositories/IApplicationUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using Kaizen.Core.Domain;
+using Kaizen.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+
+namespace Kaizen.Domain.Repositories
+{
+    public interface IApplicationUserRepository : IRepositoryBase<ApplicationUser, string>
+    {
+        Task<SignInResult> Login(ApplicationUser user, string password);
+    }
+}
