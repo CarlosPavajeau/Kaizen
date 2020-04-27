0b382823c82bbcb5c18841a77fe21d0ed7427f44
Carlos Pavajeau <cantte098@gmail.com>
2020-04-27 14:39:50 -0500

    BackEnd/Domain: Add IUnitWork interface

---
+++ Domain/Repositories/IUnitWork.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Kaizen.Domain.Repositories
+{
+    public interface IUnitWork : IDisposable
+    {
+        IClientsRepository Clients { get; }
+        IApplicationUserRepository ApplicationUsers { get; }
+        Task Save();
+    }
+}
