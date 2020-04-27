5fa6858311e6e385452331aa936ce6e13eefd29d
Carlos Pavajeau <cantte098@gmail.com>
2020-04-27 14:32:17 -0500

    BackEnd/Core: Add IRepositoryBase interface

---
+++ Core/Domain/IRepositoryBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Kaizen.Core.Domain
+{
+    public interface IRepositoryBase<T, TKey> where T : class
+    {
+        IQueryable<T> GetAll();
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);
+
+        void Insert(T entity);
+        T FindById(TKey id);
+        Task<T> FindByIdAsync(TKey id);
+        void Update(T entity);
+
+    }
+}
