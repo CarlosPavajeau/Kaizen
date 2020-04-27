141898d85532f37e312cb2a8c9e8ac3528e4a051
Carlos Pavajeau <cantte098@gmail.com>
2020-04-27 14:42:07 -0500

    BackEnd/Domain: Add ClientsRepository class

---
+++ Domain/Repositories/ClientsRepository.cs
@@ -0,0 +1,13 @@
+﻿using Kaizen.Domain.Data;
+using Kaizen.Domain.Entities;
+
+namespace Kaizen.Domain.Repositories
+{
+    public class ClientsRepository : RepositoryBase<Client, string>, IClientsRepository
+    {
+        public ClientsRepository(ApplicationDbContext applicationDbContext) : base(applicationDbContext)
+        {
+
+        }
+    }
+}
