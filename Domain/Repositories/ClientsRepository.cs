0674b899f28a7a1e55ec237e2dc95ba9e2cda4f4
Carlos Pavajeau <cantte098@gmail.com>
2020-04-28 19:59:29 -0500

    BackEnd/Domain/Repositories: Add new ClientsRepository methods

--- Domain/Repositories/ClientsRepository.cs
+++ Domain/Repositories/ClientsRepository.cs
@@ -1,5 +1,9 @@
 ﻿using Kaizen.Domain.Data;
 using Kaizen.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Kaizen.Domain.Repositories
 {
@@ -8,6 +12,21 @@ namespace Kaizen.Domain.Repositories
         public ClientsRepository(ApplicationDbContext applicationDbContext) : base(applicationDbContext)
         {
 
+        }
+
+        public async Task<ClientAddress> GetClientAddressAsync(string clientId)
+        {
+            return await ApplicationDbContext.ClientAddresses.Where(c => c.ClientId == clientId).FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<ContactPerson>> GetClientContactPeopleAsync(string clientId)
+        {
+            return await ApplicationDbContext.ContactPeople.Where(p => p.ClientId == clientId).ToListAsync();
+        }
+
+        public void UpdateClientAddress(ClientAddress clientAddress)
+        {
+            ApplicationDbContext.Entry(clientAddress).State = EntityState.Modified;
         }
     }
 }
--- Domain/Repositories/IClientsRepository.cs
+++ Domain/Repositories/IClientsRepository.cs
@@ -1,9 +1,14 @@
 ﻿using Kaizen.Core.Domain;
 using Kaizen.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Kaizen.Domain.Repositories
 {
     public interface IClientsRepository : IRepositoryBase<Client, string>
     {
+        Task<ClientAddress> GetClientAddressAsync(string clientId);
+        void UpdateClientAddress(ClientAddress clientAddress);
+        Task<IEnumerable<ContactPerson>> GetClientContactPeopleAsync(string clientId);
     }
 }
