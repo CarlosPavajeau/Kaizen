06b5151bfbe00ab81b3ebd75377fba24adf60f51
Carlos Pavajeau <cantte098@gmail.com>
2020-04-28 19:58:08 -0500

    BackEnd: Use EntityTypeConfigurations classes in ApplicationDbContext

--- Domain/Data/ApplicationDbContext.cs
+++ Domain/Data/ApplicationDbContext.cs
@@ -1,5 +1,5 @@
-﻿using Kaizen.Domain.Entities;
-using Microsoft.AspNetCore.Identity;
+﻿using Kaizen.Domain.Data.Configuration.EntityTypeConfigurations;
+using Kaizen.Domain.Entities;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,42 +16,11 @@ namespace Kaizen.Domain.Data
         {
             base.OnModelCreating(builder);
 
-            builder.Entity<ApplicationUser>(u =>
-            {
-                u.Property(p => p.Id).HasMaxLength(191);
-                u.Property(p => p.UserName).HasMaxLength(15);
-                u.Property(p => p.NormalizedUserName).HasMaxLength(15);
-                u.Property(p => p.PasswordHash).HasMaxLength(191);
-                u.Property(p => p.PhoneNumber).HasMaxLength(10);
-                u.Property(p => p.Email).HasMaxLength(150);
-                u.Property(p => p.NormalizedEmail).HasMaxLength(150);
-            });
-
-            builder.Entity<IdentityUserLogin<string>>(l =>
-            {
-                l.Property(p => p.LoginProvider).HasMaxLength(128);
-                l.Property(p => p.ProviderKey).HasMaxLength(128);
-            });
-
-            builder.Entity<IdentityUserToken<string>>(t =>
-            {
-                t.Property(p => p.LoginProvider).HasMaxLength(128);
-                t.Property(p => p.Name).HasMaxLength(128);
-            });
-
-            builder.Entity<IdentityRole>(r =>
-            {
-                r.Property(p => p.Name).HasMaxLength(191);
-                r.Property(p => p.NormalizedName).HasMaxLength(191);
-                r.Property(p => p.Id).HasMaxLength(30);
-            });
-
-            builder.Entity<Client>(c =>
-            {
-                c.HasIndex(i => i.NIT)
-                    .IsUnique();
-                c.Property("UserId").HasMaxLength(191);
-            });
+            builder.ApplyConfiguration(new ApplicationUserConfiguration());
+            builder.ApplyConfiguration(new IdentityUserLoginConfiguration());
+            builder.ApplyConfiguration(new IdentityUserTokenConfiguration());
+            builder.ApplyConfiguration(new IdentityRoleConfiguration());
+            builder.ApplyConfiguration(new ClientConfiguration());
         }
 
         public DbSet<Client> Clients { get; set; }
