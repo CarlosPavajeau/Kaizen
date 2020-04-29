95a985a9c2be372ad007d786af376be459c7f5e3
Carlos Pavajeau <cantte098@gmail.com>
2020-04-29 18:49:28 -0500

    FrontEnd/Clients: Make client table

--- Kaizen/ClientApp/src/app/modules/clients/components/clients/clients.component.ts
+++ Kaizen/ClientApp/src/app/modules/clients/components/clients/clients.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+
+import { Client } from "@modules/clients/models/client";
+import { ClientService } from "@modules/clients/services/client.service";
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-clients',
   templateUrl: './clients.component.html',
   styleUrls: ['./clients.component.css']
 })
-export class ClientsComponent implements OnInit {
+export class ClientsComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  clients: Client[];
+  dataSource: MatTableDataSource<Client> = new MatTableDataSource<Client>(this.clients);
+  displayedColumns: string[] = ['id', 'name'];
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
-  ngOnInit(): void {
+  constructor(
+    private clientService: ClientService
+  ) { }
+
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
   }
 
+  ngOnInit(): void {
+    this.loadClients();
+  }
+
+  loadClients(): void {
+    this.clientService.getClients().subscribe(clients => {
+      this.clients = clients;
+      this.dataSource = new MatTableDataSource<Client>(clients);
+    });
+  }
+
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim().toLowerCase();
+    this.dataSource.filter = filterValue;
+  }
 }
