af9294209e43a5e0a41b43837733ab7b5a16975f
Carlos Pavajeau <cantte098@gmail.com>
2020-04-28 20:12:36 -0500

    FrontEnd/Core/Interceptors: Remove retry call

--- Kaizen/ClientApp/src/app/core/interceptors/http-error.interceptor.ts
+++ Kaizen/ClientApp/src/app/core/interceptors/http-error.interceptor.ts
@@ -22,7 +22,6 @@ export class HttpErrorInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request)
       .pipe(
-        retry(1),
         catchError((error: HttpErrorResponse) => {
           if (error.status === 401)
           {
