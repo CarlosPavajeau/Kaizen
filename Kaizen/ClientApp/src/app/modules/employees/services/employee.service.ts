7012aa9b3ee4bbd42301e93553c2371e26039730
Carlos Pavajeau <cantte098@gmail.com>
2020-04-30 22:06:33 -0500

    FrontEnd/Employees: Make Employee service (#3)

--- Kaizen/ClientApp/src/app/modules/employees/services/employee.service.ts
+++ Kaizen/ClientApp/src/app/modules/employees/services/employee.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Employee } from '../models/employee';
+import { Endpoints } from '@app/global/endpoints';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class EmployeeService {
+	constructor(private http: HttpClient) {}
 
-  constructor() { }
+	getEmployees(): Observable<Employee[]> {
+		return this.http.get<Employee[]>(Endpoints.EmployeesUrl);
+	}
+
+	getEmployee(id: string): Observable<Employee> {
+		return this.http.get<Employee>(`${Endpoints.EmployeesUrl}/${id}`);
+	}
+
+	saveEmployee(employee: Employee): Observable<Employee> {
+		return this.http.post<Employee>(Endpoints.EmployeesUrl, employee);
+	}
+
+	updateEmployee(employee: Employee): Observable<Employee> {
+		return this.http.put<Employee>(Endpoints.EmployeesUrl, employee);
+	}
 }
