06936b4abaead9db4e184ae014b7a8c983a7f1cc
Carlos Pavajeau <cantte098@gmail.com>
2020-05-01 14:26:29 -0500

    FrontEnd/Global: Add Services, Products and Equipments Urls

--- Kaizen/ClientApp/src/app/global/endpoints.ts
+++ Kaizen/ClientApp/src/app/global/endpoints.ts
@@ -10,4 +10,16 @@ export class Endpoints {
 	public static get EmployeesUrl(): string {
 		return 'api/Employees';
 	}
+
+	public static get ServicesUrl(): string {
+		return 'api/Services';
+	}
+
+	public static get ProductsUrl(): string {
+		return 'api/Products';
+	}
+
+	public static get EquipmentsUrl(): string {
+		return 'api/Equipments';
+	}
 }
