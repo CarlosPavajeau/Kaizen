c73720c9600f8c8fdc70345cd18d77587697e92c
Carlos Pavajeau <cantte098@gmail.com>
2020-04-30 22:07:13 -0500

    FrontEnd/Employees: Format file (#3)

--- Kaizen/ClientApp/src/app/modules/employees/components/employee-register/employee-register.component.ts
+++ Kaizen/ClientApp/src/app/modules/employees/components/employee-register/employee-register.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-employee-register',
-  templateUrl: './employee-register.component.html',
-  styleUrls: ['./employee-register.component.css']
+	selector: 'app-employee-register',
+	templateUrl: './employee-register.component.html',
+	styleUrls: [ './employee-register.component.css' ]
 })
 export class EmployeeRegisterComponent implements OnInit {
+	constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {}
 }
