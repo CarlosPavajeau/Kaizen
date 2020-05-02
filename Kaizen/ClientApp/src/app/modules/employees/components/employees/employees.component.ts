3a3ed42478c87357770f03e03f8a66baa17afa42
Carlos Pavajeau <cantte098@gmail.com>
2020-05-02 10:21:52 -0500

    FrontEnd/Employees: Make employees component (#3)

--- Kaizen/ClientApp/src/app/modules/employees/components/employees/employees.component.ts
+++ Kaizen/ClientApp/src/app/modules/employees/components/employees/employees.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+
+import { Employee } from '@modules/employees/models/employee';
+import { EmployeeService } from '@modules/employees/services/employee.service';
 
 @Component({
-  selector: 'app-employees',
-  templateUrl: './employees.component.html',
-  styleUrls: ['./employees.component.css']
+	selector: 'app-employees',
+	templateUrl: './employees.component.html',
+	styleUrls: [ './employees.component.css' ]
 })
-export class EmployeesComponent implements OnInit {
+export class EmployeesComponent implements OnInit, AfterViewInit {
+	employees: Employee[];
+	dataSource: MatTableDataSource<Employee> = new MatTableDataSource<Employee>(this.employees);
+	displayedColumns: string[] = [ 'id', 'name' ];
+	@ViewChild(MatPaginator, { static: true })
+	paginator: MatPaginator;
+	@ViewChild(MatSort) sort: MatSort;
 
-  constructor() { }
+	constructor(private employeeService: EmployeeService) {}
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+		this.loadEmployees();
+	}
 
+	private loadEmployees() {
+		this.employeeService.getEmployees().subscribe((employees) => {
+			this.employees = employees;
+			this.dataSource.data = this.employees;
+		});
+	}
+
+	ngAfterViewInit(): void {
+		this.dataSource.paginator = this.paginator;
+		this.dataSource.sort = this.sort;
+	}
+
+	applyFilter(filterValue: string) {
+		filterValue = filterValue.trim().toLowerCase();
+		this.dataSource.filter = filterValue;
+	}
 }
