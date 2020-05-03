bd031f8b78bc27dc8414112bd2e14c17ddf4c27b
Carlos Pavajeau <cantte098@gmail.com>
2020-05-03 16:06:45 -0500

    BackEnd/Controllers/Employees: Remove unnecesary filter attributes

--- Kaizen/Controllers/EmployeesController.cs
+++ Kaizen/Controllers/EmployeesController.cs
@@ -1,168 +1,165 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using AutoMapper;
 using Kaizen.Domain.Entities;
 using Kaizen.Domain.Repositories;
 using Kaizen.EditModels;
-using Kaizen.Filters;
 using Kaizen.InputModels;
 using Kaizen.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Kaizen.Controllers
 {
-	[Authorize]
-	[Route("api/[controller]")]
-	[ApiController]
-	public class EmployeesController : ControllerBase
-	{
-		private readonly IEmployeesRepository _employeesRepository;
-		private readonly IUnitWork _unitWork;
-		private readonly IMapper _mapper;
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly IEmployeesRepository _employeesRepository;
+        private readonly IUnitWork _unitWork;
+        private readonly IMapper _mapper;
 
-		public EmployeesController(IEmployeesRepository employeesRepository, IUnitWork unitWork, IMapper mapper)
-		{
-			_employeesRepository = employeesRepository;
-			_unitWork = unitWork;
-			_mapper = mapper;
-		}
+        public EmployeesController(IEmployeesRepository employeesRepository, IUnitWork unitWork, IMapper mapper)
+        {
+            _employeesRepository = employeesRepository;
+            _unitWork = unitWork;
+            _mapper = mapper;
+        }
 
-		// GET: api/Employees
-		[HttpGet]
-		public async Task<ActionResult<IEnumerable<EmployeeViewModel>>> GetEmployees()
-		{
-			return await _employeesRepository.GetAll().Select(p => _mapper.Map<EmployeeViewModel>(p)).ToListAsync();
-		}
+        // GET: api/Employees
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<EmployeeViewModel>>> GetEmployees()
+        {
+            return await _employeesRepository.GetAll().Select(p => _mapper.Map<EmployeeViewModel>(p)).ToListAsync();
+        }
 
-		[HttpGet("[action]")]
-		[AllowAnonymous]
-		[ServiceFilter(typeof(ModelStateFilterAttribute))]
-		public async Task<ActionResult<IEnumerable<EmployeeChargeViewModel>>> EmployeeCharges()
-		{
-			return await _employeesRepository.GetAllEmployeeCharges().Select(p => _mapper.Map<EmployeeChargeViewModel>(p)).ToListAsync();
-		}
+        [HttpGet("[action]")]
+        [AllowAnonymous]
+        public async Task<ActionResult<IEnumerable<EmployeeChargeViewModel>>> EmployeeCharges()
+        {
+            return await _employeesRepository.GetAllEmployeeCharges().Select(p => _mapper.Map<EmployeeChargeViewModel>(p)).ToListAsync();
+        }
 
-		// GET: api/Employees/5
-		[HttpGet("{id}")]
-		public async Task<ActionResult<EmployeeViewModel>> GetEmployee(string id)
-		{
-			Employee employee = await _employeesRepository.FindByIdAsync(id);
+        // GET: api/Employees/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<EmployeeViewModel>> GetEmployee(string id)
+        {
+            Employee employee = await _employeesRepository.FindByIdAsync(id);
 
-			if (employee == null)
-			{
-				return NotFound();
-			}
+            if (employee == null)
+            {
+                return NotFound();
+            }
 
-			return _mapper.Map<EmployeeViewModel>(employee);
-		}
+            return _mapper.Map<EmployeeViewModel>(employee);
+        }
 
-		[HttpGet("[action]/{id}")]
-		[AllowAnonymous]
-		[ServiceFilter(typeof(ModelStateFilterAttribute))]
-		public async Task<ActionResult<bool>> CheckEmployeeExists(string id)
-		{
-			return await _employeesRepository.GetAll().AnyAsync(c => c.Id == id);
-		}
+        [HttpGet("[action]/{id}")]
+        [AllowAnonymous]
+        public async Task<ActionResult<bool>> CheckEmployeeExists(string id)
+        {
+            return await _employeesRepository.GetAll().AnyAsync(c => c.Id == id);
+        }
 
-		// PUT: api/Employees/5
-		// To protect from overposting attacks, enable the specific properties you want to bind to, for
-		// more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
-		[HttpPut("{id}")]
-		public async Task<IActionResult> PutEmployee(string id, EmployeeEditModel employeeModel)
-		{
-			Employee employee = await _employeesRepository.FindByIdAsync(id);
+        // PUT: api/Employees/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutEmployee(string id, EmployeeEditModel employeeModel)
+        {
+            Employee employee = await _employeesRepository.FindByIdAsync(id);
 
-			if (employee is null)
-			{
-				return BadRequest();
-			}
+            if (employee is null)
+            {
+                return BadRequest();
+            }
 
-			_mapper.Map(employeeModel, employee);
-			_employeesRepository.Update(employee);
+            _mapper.Map(employeeModel, employee);
+            _employeesRepository.Update(employee);
 
-			try
-			{
-				await _unitWork.SaveAsync();
-			}
-			catch (DbUpdateConcurrencyException)
-			{
-				if (!EmployeeExists(id))
-				{
-					return NotFound();
-				}
-				else
-				{
-					throw;
-				}
-			}
+            try
+            {
+                await _unitWork.SaveAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EmployeeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
 
-			return NoContent();
-		}
+            return NoContent();
+        }
 
-		// POST: api/Employees
-		// To protect from overposting attacks, enable the specific properties you want to bind to, for
-		// more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
-		[HttpPost]
-		public async Task<ActionResult<EmployeeViewModel>> PostEmployee(EmployeeInputModel employeeModel)
-		{
-			ApplicationUser applicationUser = await _unitWork.ApplicationUsers.FindByIdAsync(employeeModel.UserId);
-			if (applicationUser is null)
-			{
-				return BadRequest();
-			}
+        // POST: api/Employees
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<EmployeeViewModel>> PostEmployee(EmployeeInputModel employeeModel)
+        {
+            ApplicationUser applicationUser = await _unitWork.ApplicationUsers.FindByIdAsync(employeeModel.UserId);
+            if (applicationUser is null)
+            {
+                return BadRequest();
+            }
 
-			EmployeeCharge employeeCharge = await _employeesRepository.GetAllEmployeeCharges().Where(c => c.Id == employeeModel.ChargeId).FirstOrDefaultAsync();
-			if (employeeCharge is null)
-			{
-				return BadRequest();
-			}
+            EmployeeCharge employeeCharge = await _employeesRepository.GetAllEmployeeCharges().Where(c => c.Id == employeeModel.ChargeId).FirstOrDefaultAsync();
+            if (employeeCharge is null)
+            {
+                return BadRequest();
+            }
 
-			Employee employee = _mapper.Map<Employee>(employeeModel);
-			employee.User = applicationUser;
-			employee.EmployeeCharge = employeeCharge;
+            Employee employee = _mapper.Map<Employee>(employeeModel);
+            employee.User = applicationUser;
+            employee.EmployeeCharge = employeeCharge;
 
-			_employeesRepository.Insert(employee);
+            _employeesRepository.Insert(employee);
 
-			try
-			{
-				await _unitWork.SaveAsync();
-			}
-			catch (DbUpdateException)
-			{
-				if (EmployeeExists(employee.Id))
-				{
-					return Conflict();
-				}
-				else
-				{
-					throw;
-				}
-			}
+            try
+            {
+                await _unitWork.SaveAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (EmployeeExists(employee.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
 
-			return _mapper.Map<EmployeeViewModel>(employee);
-		}
+            return _mapper.Map<EmployeeViewModel>(employee);
+        }
 
-		// DELETE: api/Employees/5
-		[HttpDelete("{id}")]
-		public async Task<ActionResult<EmployeeViewModel>> DeleteEmployee(string id)
-		{
-			Employee employee = await _employeesRepository.FindByIdAsync(id);
-			if (employee == null)
-			{
-				return NotFound();
-			}
+        // DELETE: api/Employees/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<EmployeeViewModel>> DeleteEmployee(string id)
+        {
+            Employee employee = await _employeesRepository.FindByIdAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
 
-			await _unitWork.SaveAsync();
+            await _unitWork.SaveAsync();
 
-			return _mapper.Map<EmployeeViewModel>(employee);
-		}
+            return _mapper.Map<EmployeeViewModel>(employee);
+        }
 
-		private bool EmployeeExists(string id)
-		{
-			return _employeesRepository.GetAll().Any(p => p.Id == id);
-		}
-	}
+        private bool EmployeeExists(string id)
+        {
+            return _employeesRepository.GetAll().Any(p => p.Id == id);
+        }
+    }
 }
