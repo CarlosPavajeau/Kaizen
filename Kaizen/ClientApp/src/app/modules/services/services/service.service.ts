33bc57201a08c7e4b79a1da7e50df26d1b40f8fa
Carlos Pavajeau <cantte098@gmail.com>
2020-05-02 19:23:57 -0500

    FrontEnd/Services: Make service Service, yes is ambiguous (#6)

--- Kaizen/ClientApp/src/app/modules/services/services/service.service.ts
+++ Kaizen/ClientApp/src/app/modules/services/services/service.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { SERVICES_API_URL } from '@global/endpoints';
+import { Service } from '@modules/services/models/service';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class ServiceService {
+	constructor(private http: HttpClient) {}
 
-  constructor() { }
+	getServices(): Observable<Service[]> {
+		return this.http.get<Service[]>(SERVICES_API_URL);
+	}
+
+	getService(id: string): Observable<Service> {
+		return this.http.get<Service>(`${SERVICES_API_URL}/${id}`);
+	}
+
+	saveService(service: Service): Observable<Service> {
+		return this.http.post<Service>(SERVICES_API_URL, service);
+	}
+
+	updateService(service: Service): Observable<Service> {
+		return this.http.put<Service>(`${SERVICES_API_URL}/${service.code}`, service);
+	}
 }
