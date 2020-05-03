b63bacdccfa319c01e1d5d25f61097ec78188cc3
Carlos Pavajeau <cantte098@gmail.com>
2020-05-03 16:01:26 -0500

    BackEnd: Add ExceptionMiddleware class

---
+++ Kaizen/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using Kaizen.Core.Exceptions;
+using Kaizen.ViewModels;
+using Microsoft.AspNetCore.Http;
+
+namespace Kaizen.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception exception)
+            {
+
+                await HandleExceptionAsync(httpContext, exception);
+            }
+        }
+
+        public Task HandleExceptionAsync(HttpContext httpContext, Exception exception)
+        {
+            httpContext.Response.ContentType = "application/json";
+
+            if (exception is HttpException httpException)
+            {
+                httpContext.Response.StatusCode = httpException.StatusCode;
+            }
+            else
+            {
+                httpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            }
+
+            return httpContext.Response.WriteAsync(new ErrorDetail
+            {
+                StatusCode = httpContext.Response.StatusCode,
+                Message = exception.Message
+
+            }.ToString());
+
+        }
+    }
+}
