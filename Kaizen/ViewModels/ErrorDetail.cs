96af1efff9beee6de550d430128f697301151fab
Carlos Pavajeau <cantte098@gmail.com>
2020-05-03 16:02:59 -0500

    BackEnd/ViewModels: Add ErrorDetail class

---
+++ Kaizen/ViewModels/ErrorDetail.cs
@@ -0,0 +1,15 @@
+using Newtonsoft.Json;
+
+namespace Kaizen.ViewModels
+{
+    public class ErrorDetail
+    {
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
