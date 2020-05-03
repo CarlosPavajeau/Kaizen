f350eddbb8ca9a31cccec87508756c127ba64109
Carlos Pavajeau <cantte098@gmail.com>
2020-05-02 19:22:39 -0500

    FrontEnd/Services: Add Service and ServiceType model (#6)

---
+++ Kaizen/ClientApp/src/app/modules/services/models/service-type.ts
@@ -0,0 +1,4 @@
+export interface ServiceType {
+	id: number;
+	name: string;
+}
--- Kaizen/ClientApp/src/app/modules/services/models/service.ts
+++ Kaizen/ClientApp/src/app/modules/services/models/service.ts
@@ -1,2 +1,9 @@
+import { ServiceType } from '@modules/services/models/service-type';
+
 export interface Service {
+	code: string;
+	name: string;
+	cost: number;
+	serviceTypeId: number;
+	serviceType?: ServiceType;
 }
