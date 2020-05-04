e0b5cc58743542d9d246cfc2f86978056ec05dce
Carlos Pavajeau <cantte098@gmail.com>
2020-05-04 11:53:50 -0500

    FrontEnd/Core/HttErrorInterceptor: Add new status code

--- Kaizen/ClientApp/src/app/core/interceptors/http-error.interceptor.ts
+++ Kaizen/ClientApp/src/app/core/interceptors/http-error.interceptor.ts
@@ -12,9 +12,9 @@ export class HttpErrorInterceptor implements HttpInterceptor {
 	intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 		return next.handle(request).pipe(
 			catchError((error: HttpErrorResponse) => {
-				if (error.status === 401) {
+				if ([ 401, 403 ].includes(error.status)) {
 					this.authService.logoutUser();
-					this.router.navigateByUrl('/user/login');
+          window.location.reload();
 				}
 				return throwError(error);
 			})
