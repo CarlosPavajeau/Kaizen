d53f890d2a6a76d72a68763ebfa3fe9a87d1bfac
Carlos Pavajeau <cantte098@gmail.com>
2020-05-04 12:06:25 -0500

    FrontEnd/Employees: Add AuthGuard and AdminGuard (#3)

--- Kaizen/ClientApp/src/app/modules/employees/employees-routing.module.ts
+++ Kaizen/ClientApp/src/app/modules/employees/employees-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { EmployeeRegisterComponent } from './components/employee-register/employee-register.component';
 import { EmployeesComponent } from './components/employees/employees.component';
+import { AuthGuard } from '@core/guards/auth.guard';
+import { AdminGuard } from '@core/guards/admin.guard';
 
 const routes: Routes = [
 	{
 		path: '',
 		children: [
-			{ path: 'register', component: EmployeeRegisterComponent },
-			{ path: 'employees', component: EmployeesComponent },
+			{ path: 'register', component: EmployeeRegisterComponent, canActivate: [ AuthGuard, AdminGuard ] },
+			{ path: 'employees', component: EmployeesComponent, canActivate: [ AuthGuard, AdminGuard ] },
 			{ path: '', redirectTo: 'employees', pathMatch: 'full' }
 		]
 	}
