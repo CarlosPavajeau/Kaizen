467bff749e4111bf696ed3be546116d6e8eb4e2f
Carlos Pavajeau <cantte098@gmail.com>
2020-05-04 11:47:03 -0500

    BackEnd/Infrastructure/Security: Add new Claim, roleName

--- Infrastructure/Security/JwtSecurityTokenGenerator.cs
+++ Infrastructure/Security/JwtSecurityTokenGenerator.cs
@@ -1,15 +1,16 @@
-﻿using Microsoft.Extensions.Configuration;
-using Microsoft.IdentityModel.Tokens;
 using System;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Kaizen.Infrastructure.Security
 {
     public static class JwtSecurityTokenGenerator
     {
-        public static string GenerateSecurityToken(IConfiguration configuration, string username)
+        public static string GenerateSecurityToken(IConfiguration configuration, string username, string roleName)
         {
             JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
             byte[] key = Encoding.ASCII.GetBytes(configuration["AppSettings:Key"]);
@@ -18,7 +19,8 @@ namespace Kaizen.Infrastructure.Security
             {
                 Subject = new ClaimsIdentity(new Claim[]
                 {
-                    new Claim(ClaimTypes.Name, username)
+                    new Claim(ClaimTypes.Name, username),
+                    new Claim(new IdentityOptions().ClaimsIdentity.RoleClaimType, roleName)
                 }),
                 Expires = DateTime.Now.AddDays(7),
                 SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
