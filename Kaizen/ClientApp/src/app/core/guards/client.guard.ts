93bd0749c5e8cc2d64499b3f91690c2f35c4098a
Carlos Pavajeau <cantte098@gmail.com>
2020-05-04 12:00:35 -0500

    FrontEnd/Core/Guards: Implement admin, client and employee guards

--- Kaizen/ClientApp/src/app/core/guards/client.guard.ts
+++ Kaizen/ClientApp/src/app/core/guards/client.guard.ts
@@ -1,15 +1,24 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthenticationService } from '@core/authentication/authentication.service';
+import { CLIENT } from '@global/roles';
 
 @Injectable({
 	providedIn: 'root'
 })
 export class ClientGuard implements CanActivate {
+	constructor(private authService: AuthenticationService, private router: Router) {}
+
 	canActivate(
 		next: ActivatedRouteSnapshot,
 		state: RouterStateSnapshot
 	): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+		var role = this.authService.getUserRole();
+		if (role !== CLIENT) {
+			this.router.navigateByUrl('/user/profile');
+			return false;
+		}
 		return true;
 	}
 }
