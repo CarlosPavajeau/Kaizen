d3e9eb04b29408fbe79fea886b9a388e680d8eac
Carlos Pavajeau <cantte098@gmail.com>
2020-05-06 12:11:26 -0500

    FrontEnd/Inventory/Equipments: Make equipment register form (#4)

--- Kaizen/ClientApp/src/app/modules/inventory/equipments/components/equipment-register/equipment-register.component.ts
+++ Kaizen/ClientApp/src/app/modules/inventory/equipments/components/equipment-register/equipment-register.component.ts
@@ -1,15 +1,60 @@
 import { Component, OnInit } from '@angular/core';
+import { EquipmentService } from '@modules/inventory/equipments/services/equipment.service';
+import { FormBuilder, FormGroup, AbstractControl, Validators } from '@angular/forms';
+import { IForm } from '@core/models/form';
+import { Equipment } from '@modules/inventory/equipments/models/equipment';
+import { NotificationsService } from '@app/shared/services/notifications.service';
+import { EquipmentExistsValidator } from '@app/shared/validators/equipment-exists-validator';
 
 @Component({
-  selector: 'app-equipment-register',
-  templateUrl: './equipment-register.component.html',
-  styleUrls: ['./equipment-register.component.css']
+	selector: 'app-equipment-register',
+	templateUrl: './equipment-register.component.html',
+	styleUrls: [ './equipment-register.component.css' ]
 })
-export class EquipmentRegisterComponent implements OnInit {
+export class EquipmentRegisterComponent implements OnInit, IForm {
+	equipmentForm: FormGroup;
 
-  constructor() { }
+	public get controls(): { [key: string]: AbstractControl } {
+		return this.equipmentForm.controls;
+	}
 
-  ngOnInit(): void {
-  }
+	constructor(
+		private equipmentService: EquipmentService,
+		private formBuilder: FormBuilder,
+		private equipmentValidator: EquipmentExistsValidator,
+		private notificationService: NotificationsService
+	) {}
 
+	ngOnInit(): void {
+		this.initForm();
+	}
+
+	initForm(): void {
+		this.equipmentForm = this.formBuilder.group({
+			code: [
+				'',
+				{
+					validators: [ Validators.required, Validators.minLength(3), Validators.maxLength(20) ],
+					asyncValidators: [ this.equipmentValidator.validate.bind(this.equipmentValidator) ],
+					updateOn: 'blur'
+				}
+			],
+			name: [ '', [ Validators.required, Validators.minLength(5), Validators.maxLength(50) ] ],
+			maintenanceDate: [ new Date(), [ Validators.required ] ]
+		});
+	}
+
+	onSubmit() {
+		if (this.equipmentForm.valid) {
+			const equipment: Equipment = {
+				code: this.controls['code'].value,
+				name: this.controls['name'].value,
+				maintenanceDate: this.controls['maintenanceDate'].value
+			};
+
+			this.equipmentService.saveEquipment(equipment).subscribe((equipmentSave) => {
+				this.notificationService.add(`El equipo ${equipmentSave.name} ha sido registrado`, 'OK');
+			});
+		}
+	}
 }
--- Kaizen/ClientApp/src/app/modules/inventory/equipments/equipments.module.ts
+++ Kaizen/ClientApp/src/app/modules/inventory/equipments/equipments.module.ts
@@ -4,9 +4,11 @@ import { CommonModule } from '@angular/common';
 import { EquipmentsRoutingModule } from './equipments-routing.module';
 import { EquipmentRegisterComponent } from './components/equipment-register/equipment-register.component';
 import { EquipmentsComponent } from './components/equipments/equipments.component';
+import { SharedModule } from '@app/shared/shared.module';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
-	declarations: [EquipmentRegisterComponent, EquipmentsComponent],
-	imports: [ CommonModule, EquipmentsRoutingModule ]
+	declarations: [ EquipmentRegisterComponent, EquipmentsComponent ],
+	imports: [ CommonModule, EquipmentsRoutingModule, SharedModule, FormsModule, ReactiveFormsModule ]
 })
 export class EquipmentsModule {}
