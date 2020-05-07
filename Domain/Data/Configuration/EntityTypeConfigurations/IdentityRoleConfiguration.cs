8b814c30e13a32685315366c4b50772d82f911ae
Carlos Pavajeau <cantte098@gmail.com>
2020-05-06 21:38:59 -0500

    BackEnd/Domain: Fix IdentityRole 'Id' and 'ConcurrencyStamp' regenerated

--- Domain/Data/Configuration/EntityTypeConfigurations/IdentityRoleConfiguration.cs
+++ Domain/Data/Configuration/EntityTypeConfigurations/IdentityRoleConfiguration.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
@@ -16,23 +16,31 @@ namespace Kaizen.Domain.Data.Configuration.EntityTypeConfigurations
             {
                 new IdentityRole
                 {
+                    Id = "3bb4b79d-85a4-4a94-b55e-5619c9acf4a2",
                     Name = "Administrator",
-                    NormalizedName = "ADMINISTRATOR"
+                    NormalizedName = "ADMINISTRATOR",
+                    ConcurrencyStamp = "1ed77447-fe5c-42c2-9711-3f91cc103255"
                 },
                 new IdentityRole
                 {
+                    Id = "e88f6181-e86a-49e1-a2da-c79c71914624",
                     Name = "OfficeEmployee",
-                    NormalizedName = "OFFICEEMPLOYEE"
+                    NormalizedName = "OFFICEEMPLOYEE",
+                    ConcurrencyStamp = "177cda8b-1541-411e-8891-62f58b0e45fa"
                 },
                 new IdentityRole
                 {
+                    Id = "e6728857-7423-443f-8228-2c8dd22f3aab",
                     Name = "TechnicalEmployee",
-                    NormalizedName = "TECHNICALEMPLOYEE"
+                    NormalizedName = "TECHNICALEMPLOYEE",
+                    ConcurrencyStamp = "501614ae-a5ad-4ee3-ba6f-17c28ab1cd5d"
                 },
                 new IdentityRole
                 {
+                    Id = "a988a9ea-c7a5-4329-aceb-3da5016c6a43",
                     Name = "Client",
-                    NormalizedName = "CLIENT"
+                    NormalizedName = "CLIENT",
+                    ConcurrencyStamp = "fba45aab-42d7-4e12-9dc0-44a2f68badf1"
                 }
             });
         }
