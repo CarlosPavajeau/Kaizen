76ad94f19d551a068b55feae3f89086641d78dce
Carlos Pavajeau <cantte098@gmail.com>
2020-05-10 09:42:36 -0500

    FrontEnd/Inventory/Products: Make product register form (#4)

--- Kaizen/ClientApp/src/app/modules/inventory/products/components/product-register/product-register.component.ts
+++ Kaizen/ClientApp/src/app/modules/inventory/products/components/product-register/product-register.component.ts
@@ -1,15 +1,117 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { IForm } from '@core/models/form';
+import { AbstractControl, FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
+import { ProductService } from '../../services/product.service';
+import { ProductExistsValidator } from '@app/shared/validators/product-exists-validator';
+import { Observable } from 'rxjs';
+import { MatAutocomplete, MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import { startWith, map } from 'rxjs/operators';
+import { MatChipInputEvent } from '@angular/material/chips';
 
 @Component({
-  selector: 'app-product-register',
-  templateUrl: './product-register.component.html',
-  styleUrls: ['./product-register.component.css']
+	selector: 'app-product-register',
+	templateUrl: './product-register.component.html',
+	styleUrls: [ './product-register.component.css' ]
 })
-export class ProductRegisterComponent implements OnInit {
+export class ProductRegisterComponent implements OnInit, IForm {
+	allMonths: string[] = [
+		'Enero',
+		'Febrero',
+		'Marzo',
+		'Abril',
+		'Mayo',
+		'Junio',
+		'Julio',
+		'Agosto',
+		'Septiembre',
+		'Octubre',
+		'Noviembre',
+		'Diciembre'
+	];
 
-  constructor() { }
+	visible = true;
+	selectable = true;
+	removable = true;
+	separatorKeysCodes: number[] = [ ENTER, COMMA ];
+	filteredMonths: Observable<string[]>;
+	applicationMonthsArray: string[] = [];
+	@ViewChild('monthInput') monthInput: ElementRef<HTMLInputElement>;
+	@ViewChild('auto') matAutocomplete: MatAutocomplete;
+	productForm: FormGroup;
 
-  ngOnInit(): void {
-  }
+	public get controls(): { [key: string]: AbstractControl } {
+		return this.productForm.controls;
+	}
 
+	constructor(
+		private productService: ProductService,
+		private formBuilder: FormBuilder,
+		private productExistsValidator: ProductExistsValidator
+	) {}
+
+	ngOnInit(): void {
+		this.initForm();
+
+		this.filteredMonths = this.controls['applicationMonths'].valueChanges.pipe(
+			startWith(null),
+			map(
+				(month: string | null) =>
+
+						month ? this._filter(month) :
+						this.allMonths.slice()
+			)
+		);
+	}
+
+	initForm(): void {
+		this.productForm = this.formBuilder.group({
+			code: [
+				'',
+				{
+					validators: [ Validators.required ],
+					asyncValidators: [ this.productExistsValidator.validate.bind(this.productExistsValidator) ]
+				}
+			],
+			healthRegister: [ '', [ Validators.required ] ],
+			amount: [ '', [ Validators.required ] ],
+			applicationMonths: [ '', [ Validators.required ] ]
+		});
+	}
+
+	onSubmit() {}
+
+	addMonth(event: MatChipInputEvent): void {
+		const input = event.input;
+		const value = event.value;
+
+		if ((value || '').trim()) {
+			this.applicationMonthsArray.push(value.trim());
+		}
+
+		if (input) {
+			input.value = '';
+		}
+
+		this.controls['applicationMonths'].setValue(null);
+	}
+
+	removeMonth(month: string) {
+		const index = this.applicationMonthsArray.indexOf(month);
+		if (index >= 0) {
+			this.applicationMonthsArray.splice(index, 1);
+		}
+	}
+
+	selectedMonth(event: MatAutocompleteSelectedEvent): void {
+		this.applicationMonthsArray.push(event.option.viewValue);
+		this.monthInput.nativeElement.value = '';
+		this.controls['applicationMonths'].setValue(null);
+	}
+
+	private _filter(value: string): string[] {
+		const filterValue = value.toLowerCase();
+
+		return this.allMonths.filter((month) => month.toLowerCase().indexOf(filterValue) === 0);
+	}
 }
--- Kaizen/ClientApp/src/app/modules/inventory/products/products.module.ts
+++ Kaizen/ClientApp/src/app/modules/inventory/products/products.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductRegisterComponent } from './components/product-register/product-register.component';
 import { ProductsComponent } from './components/products/products.component';
-
+import { SharedModule } from '@app/shared/shared.module';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [ProductRegisterComponent, ProductsComponent],
-  imports: [
-    CommonModule,
-    ProductsRoutingModule
-  ]
+	declarations: [ ProductRegisterComponent, ProductsComponent ],
+	imports: [ SharedModule, ProductsRoutingModule, ReactiveFormsModule, FormsModule ]
 })
-export class ProductsModule { }
+export class ProductsModule {}
