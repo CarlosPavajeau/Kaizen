4770013f911752a5d974ad243f0807ce74841f76
Carlos Pavajeau <cantte098@gmail.com>
2020-05-11 09:53:41 -0500

    FrontEnd/Users: Make a complete control panel for all users

--- Kaizen/ClientApp/src/app/modules/users/components/control-panel/control-panel.component.ts
+++ Kaizen/ClientApp/src/app/modules/users/components/control-panel/control-panel.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ControlPanelCard } from '@app/core/models/control-panel-card';
+import { ControlPanelCard } from '@core/models/control-panel-card';
+import { AuthenticationService } from '@core/authentication/authentication.service';
+import { CONTROL_PANEL_CARDS } from '@app/global/control-panel-cards';
 
 @Component({
 	selector: 'app-control-panel',
@@ -7,19 +9,12 @@ import { ControlPanelCard } from '@app/core/models/control-panel-card';
 	styleUrls: [ './control-panel.component.css' ]
 })
 export class ControlPanelComponent implements OnInit {
-	controlCards: ControlPanelCard[] = [
-		{
-			title: 'Datos de acceso',
-			imgUrl: 'assets/images/ecolplag_bird.png',
-			url: '/access'
-		},
-		{
-			title: 'Facturas',
-			imgUrl: 'assets/images/ecolplag_bird.png',
-			url: '/contact'
-		}
-	];
-	constructor() {}
+	controlCards: ControlPanelCard[] = [];
 
-	ngOnInit(): void {}
+	constructor(private authService: AuthenticationService) {}
+
+	ngOnInit(): void {
+		const userRole = this.authService.getUserRole();
+		this.controlCards = CONTROL_PANEL_CARDS[userRole];
+	}
 }
