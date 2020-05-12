54b960942dd8ed313e42a4d0cf750502c2137199
Carlos Pavajeau <cantte098@gmail.com>
2020-05-12 17:49:34 -0500

    BackEnd/Entities/WorkOrder: Add WorkOrderState property and change 'Observation' for 'Observations' (#9)

--- Domain/Entities/WorkOrder.cs
+++ Domain/Entities/WorkOrder.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace Kaizen.Domain.Entities
@@ -7,12 +7,13 @@ namespace Kaizen.Domain.Entities
     {
         [Required, MaxLength(40)]
         public string Sector { get; set; }
+        public WorkOrderState WorkOrderState { get; set; }
 
         public DateTime ExecutionDate { get; set; }
         public DateTime ArrivalTime { get; set; }
         public DateTime DepatureTime { get; set; }
         public DateTime Validity { get; set; }
         [MaxLength(500)]
-        public string Observation { get; set; }
+        public string Observations { get; set; }
     }
 }
