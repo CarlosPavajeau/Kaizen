d7a67063dd4781b666c245b2da840a714afa12c1
Carlos Pavajeau <cantte098@gmail.com>
2020-05-13 09:07:07 -0500

    BackEnd/Repositories/Services: Implement 'GetServiceTypesAsync' method

--- Infrastructure/Repositories/ServicesRepository.cs
+++ Infrastructure/Repositories/ServicesRepository.cs
@@ -1,7 +1,10 @@
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Kaizen.Domain.Data;
 using Kaizen.Domain.Entities;
 using Kaizen.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 namespace Kaizen.Infrastructure.Repositories
 {
@@ -14,6 +17,11 @@ namespace Kaizen.Infrastructure.Repositories
         public IQueryable<ServiceType> GetServiceTypes()
         {
             return ApplicationDbContext.ServiceTypes;
+        }
+
+        public async Task<IEnumerable<ServiceType>> GetServiceTypesAsync()
+        {
+            return await ApplicationDbContext.ServiceTypes.ToListAsync();
         }
     }
 }
