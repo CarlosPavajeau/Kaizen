d3fa56a24d2c19c49baf94b4560cee2af124f20d
Carlos Pavajeau <cantte098@gmail.com>
2020-05-16 19:16:59 -0500

    BackEnd/Domain/Migrations: Add WorkOrders migration

---
+++ Domain/Migrations/20200516220008_WorkOrders.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Kaizen.Domain.Migrations
+{
+    public partial class WorkOrders : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WorkOrders",
+                columns: table => new
+                {
+                    Code = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Sector = table.Column<string>(maxLength: 40, nullable: false),
+                    WorkOrderState = table.Column<int>(nullable: false, defaultValue: 0),
+                    ExecutionDate = table.Column<DateTime>(nullable: false),
+                    ArrivalTime = table.Column<DateTime>(nullable: false),
+                    DepatureTime = table.Column<DateTime>(nullable: false),
+                    Validity = table.Column<DateTime>(nullable: false),
+                    Observations = table.Column<string>(maxLength: 500, nullable: true),
+                    ActivityCode = table.Column<int>(nullable: false),
+                    EmployeeId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WorkOrders", x => x.Code);
+                    table.UniqueConstraint("AK_WorkOrders_ActivityCode", x => x.ActivityCode);
+                    table.ForeignKey(
+                        name: "FK_WorkOrders_Activity_ActivityCode",
+                        column: x => x.ActivityCode,
+                        principalTable: "Activity",
+                        principalColumn: "Code",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_WorkOrders_Employees_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "Employees",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WorkOrders_EmployeeId",
+                table: "WorkOrders",
+                column: "EmployeeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WorkOrders");
+        }
+    }
+}
