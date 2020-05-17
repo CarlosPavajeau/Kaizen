d7b8e533042f23a1c440cfafdc9f954d708a9793
Carlos Pavajeau <cantte098@gmail.com>
2020-05-16 19:15:59 -0500

    BackEnd/Entities/WorkOrder: Add Code, Activity and Employee properties. Eliminate unnecessary inheritance (#9)

--- Domain/Entities/WorkOrder.cs
+++ Domain/Entities/WorkOrder.cs
@@ -1,19 +1,33 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Kaizen.Domain.Entities
 {
-    public class WorkOrder : Activity
+    public class WorkOrder
     {
+        [Key]
+        public int Code { get; set; }
         [Required, MaxLength(40)]
         public string Sector { get; set; }
         public WorkOrderState WorkOrderState { get; set; }
 
+        [DataType(DataType.Date)]
         public DateTime ExecutionDate { get; set; }
         public DateTime ArrivalTime { get; set; }
         public DateTime DepatureTime { get; set; }
+
+        [DataType(DataType.Date)]
         public DateTime Validity { get; set; }
         [MaxLength(500)]
         public string Observations { get; set; }
+
+        public int ActivityCode { get; set; }
+        public string EmployeeId { get; set; }
+
+        [ForeignKey("ActivityCode")]
+        public Activity Activity { get; set; }
+        [ForeignKey("EmployeeId")]
+        public Employee Employee { get; set; }
     }
 }
