8de5a52ecde90f1cb32b2978608cc1047c8b8a44
Carlos Pavajeau <cantte098@gmail.com>
2020-05-18 12:40:33 -0500

    BackEnd/Infrastructure/Repositories/ServiceRequests: Override FindByIdAsync method

--- Infrastructure/Repositories/ServiceRequestsRepository.cs
+++ Infrastructure/Repositories/ServiceRequestsRepository.cs
@@ -1,6 +1,10 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Kaizen.Domain.Data;
 using Kaizen.Domain.Entities;
 using Kaizen.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 namespace Kaizen.Infrastructure.Repositories
 {
@@ -8,6 +12,24 @@ namespace Kaizen.Infrastructure.Repositories
     {
         public ServiceRequestsRepository(ApplicationDbContext applicationDbContext) : base(applicationDbContext)
         {
+        }
+
+        public override async Task<ServiceRequest> FindByIdAsync(int id)
+        {
+            ServiceRequest serviceRequest = await ApplicationDbContext.ServiceRequests.Include(s => s.Client)
+                .Include(s => s.ServiceRequestsServices).ThenInclude(s => s.Service)
+                .Where(s => s.Code == id).FirstOrDefaultAsync();
+
+            if (serviceRequest != null)
+            {
+                serviceRequest.Services = new List<Service>();
+                foreach (var serviceRequestService in serviceRequest.ServiceRequestsServices)
+                {
+                    serviceRequest.Services.Add(serviceRequestService.Service);
+                }
+            }
+
+            return serviceRequest;
         }
     }
 }
