3db20244e278c2c5f1b3e91bfedf09770d8b211c
Carlos Pavajeau <cantte098@gmail.com>
2020-05-20 10:43:04 -0500

    BackEnd/Mappers/Activity: Add new functionality on mapping an Activity

--- Kaizen/Mappers/ActivityMapperProfile.cs
+++ Kaizen/Mappers/ActivityMapperProfile.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using AutoMapper;
 using Kaizen.Domain.Entities;
 using Kaizen.Models.Activity;
@@ -9,7 +10,28 @@ namespace Kaizen.Mappers
         public ActivityMapperProfile()
         {
             CreateMap<ActivityEditModel, Activity>();
-            CreateMap<ActivityInputModel, Activity>();
+            CreateMap<ActivityInputModel, Activity>().AfterMap((activityModel, activity) =>
+            {
+                activity.ActivitiesServices = new List<ActivityService>();
+                foreach (var serviceCode in activityModel.ServiceCodes)
+                {
+                    activity.ActivitiesServices.Add(new ActivityService
+                    {
+                        ServiceCode = serviceCode,
+                        Activity = activity
+                    });
+                }
+
+                activity.ActivitiesEmployees = new List<ActivityEmployee>();
+                foreach (var employeeId in activityModel.EmployeeCodes)
+                {
+                    activity.ActivitiesEmployees.Add(new ActivityEmployee
+                    {
+                        EmployeeId = employeeId,
+                        Activity = activity
+                    });
+                }
+            });
             CreateMap<Activity, ActivityViewModel>();
         }
     }
