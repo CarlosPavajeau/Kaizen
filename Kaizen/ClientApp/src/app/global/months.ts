48ba6e31c73e1a4d3df979019f4582c5895eb137
Carlos Pavajeau <cantte098@gmail.com>
2020-05-20 18:22:32 -0500

    FrontEnd/Global: Create MONTHS_NAMES contant

---
+++ Kaizen/ClientApp/src/app/global/months.ts
@@ -0,0 +1,14 @@
+export const MONTHS_NAMES: string[] = [
+	'Enero',
+	'Febrero',
+	'Marzo',
+	'Abril',
+	'Mayo',
+	'Junio',
+	'Julio',
+	'Agosto',
+	'Septiembre',
+	'Octubre',
+	'Noviembre',
+	'Diciembre'
+];
