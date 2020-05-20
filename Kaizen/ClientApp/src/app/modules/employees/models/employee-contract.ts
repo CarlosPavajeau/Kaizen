3d6c18d069df6d31e86b9d632bf2caebdcc6c9ac
Carlos Pavajeau <cantte098@gmail.com>
2020-05-20 18:23:37 -0500

    FrontEnd/Employees: Add EmployeeContract model

---
+++ Kaizen/ClientApp/src/app/modules/employees/models/employee-contract.ts
@@ -0,0 +1,5 @@
+export interface EmployeeContract {
+	contractCode: string;
+	startDate: Date;
+	endDate: Date;
+}
--- Kaizen/ClientApp/src/app/modules/employees/models/employee.ts
+++ Kaizen/ClientApp/src/app/modules/employees/models/employee.ts
@@ -1,7 +1,10 @@
 import { EmployeeCharge } from './employee-charge';
 import { Person } from '@shared/models/person';
+import { EmployeeContract } from './employee-contract';
 
 export interface Employee extends Person {
 	chargeId: number;
+	contractCode: string;
 	employeeCharge?: EmployeeCharge;
+	employeeContract?: EmployeeContract;
 }
