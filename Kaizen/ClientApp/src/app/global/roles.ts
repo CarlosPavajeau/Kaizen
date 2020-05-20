51b51992d721a4d7cc30b2b4c482443d1dcc803b
Carlos Pavajeau <cantte098@gmail.com>
2020-05-20 11:24:19 -0500

    FrontEnd/Global: Add new roles

--- Kaizen/ClientApp/src/app/global/roles.ts
+++ Kaizen/ClientApp/src/app/global/roles.ts
@@ -1,3 +1,5 @@
 export const ADMINISTRATOR_ROLE = 'Administrator';
 export const CLIENT_ROLE = 'Client';
-export const EMPLOYEE_ROLE = 'TechnicalEmployee';
+export const TECHNICAL_EMPLOYEE_ROLE = 'TechnicalEmployee';
+export const OFFICE_EMPLOYEE_ROLE = 'OfficeEmployee';
+export const EMPLOYEE_ROLE = 'Employee';
