ebd54365be26b19b475c8d6364b463d4e145a3b3
Carlos Pavajeau <cantte098@gmail.com>
2020-05-20 11:14:36 -0500

    BackEnd/Infrastructure/Repositories/Activities: Override FindByIdAsync method

--- Infrastructure/Repositories/ActivitiesRepository.cs
+++ Infrastructure/Repositories/ActivitiesRepository.cs
@@ -1,13 +1,41 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Kaizen.Domain.Data;
 using Kaizen.Domain.Entities;
 using Kaizen.Domain.Repositories;
-
+using Microsoft.EntityFrameworkCore;
 namespace Kaizen.Infrastructure.Repositories
 {
     public class ActivitiesRepository : RepositoryBase<Activity, int>, IActivitiesRepository
     {
         public ActivitiesRepository(ApplicationDbContext applicationDbContext) : base(applicationDbContext)
         {
+        }
+
+        public override async Task<Activity> FindByIdAsync(int id)
+        {
+            Activity activity = await ApplicationDbContext.Activities.Include(a => a.Client)
+                .Include(a => a.ActivitiesServices)
+                .ThenInclude(a => a.Service).Include(a => a.ActivitiesEmployees).ThenInclude(a => a.Employee)
+                .Where(a => a.Code == id).FirstOrDefaultAsync();
+
+            if (activity != null)
+            {
+                activity.Services = new List<Service>();
+                foreach (ActivityService activityService in activity.ActivitiesServices)
+                {
+                    activity.Services.Add(activityService.Service);
+                }
+
+                activity.Employees = new List<Employee>();
+                foreach (ActivityEmployee activityEmployee in activity.ActivitiesEmployees)
+                {
+                    activity.Employees.Add(activityEmployee.Employee);
+                }
+            }
+
+            return activity;
         }
     }
 }
