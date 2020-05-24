daa193a17d25ca790a7fc57b6d4b5781fdca4d9a
Carlos Pavajeau <cantte098@gmail.com>
2020-05-23 19:34:48 -0500

    BackEnd/Domain/Entities/DataSheet: Add new properties, they are not necessary and will be deleted (#9)

--- Domain/Entities/DataSheet.cs
+++ Domain/Entities/DataSheet.cs
@@ -1,10 +1,16 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Kaizen.Domain.Entities
 {
     public class DataSheet
     {
+        [MaxLength(40)]
+        public string Presentation { get; set; }
+        public decimal Price { get; set; }
+        [MaxLength(350)]
+        public string Description { get; set; }
+
         [Key, MaxLength(15)]
         public string ProductCode { get; set; }
         [ForeignKey("ProductCode")]
