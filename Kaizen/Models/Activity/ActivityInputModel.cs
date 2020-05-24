7ca3a91752214426683d114a5d103e7cd6e0812b
Carlos Pavajeau <cantte098@gmail.com>
2020-05-24 16:09:43 -0500

    BackEnd/Models: Add Periodicity property in ActivityInputModel and ServiceRequestInputModel

--- Kaizen/Models/Activity/ActivityInputModel.cs
+++ Kaizen/Models/Activity/ActivityInputModel.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
+using Kaizen.Domain.Entities;
 
 namespace Kaizen.Models.Activity
 {
     public class ActivityInputModel : ActivityEditModel
     {
         public string ClientId { get; set; }
+        public PeriodicityType Periodicity { get; set; }
 
         public List<string> EmployeeCodes { get; set; }
         public List<string> ServiceCodes { get; set; }
--- Kaizen/Models/ServiceRequest/ServiceRequestInputModel.cs
+++ Kaizen/Models/ServiceRequest/ServiceRequestInputModel.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
+using Kaizen.Domain.Entities;
 
 namespace Kaizen.Models.ServiceRequest
 {
     public class ServiceRequestInputModel : ServiceRequestEditModel
     {
         public string ClientId { get; set; }
+        public PeriodicityType Periodicity { get; set; }
         public List<string> ServiceCodes { get; set; }
     }
 }
