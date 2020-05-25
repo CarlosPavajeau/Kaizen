6a95bedd7fcb39fb6458fc37a0eb5d6413fe6590
Carlos Pavajeau <cantte098@gmail.com>
2020-05-25 12:42:35 -0500

    FrontEnd/Inventory/Equipments: Add new equipments table (#4)

--- Kaizen/ClientApp/src/app/modules/inventory/equipments/components/equipments/equipments.component.ts
+++ Kaizen/ClientApp/src/app/modules/inventory/equipments/components/equipments/equipments.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { Equipment } from '@modules/inventory/equipments/models/equipment';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { EquipmentService } from '@modules/inventory/equipments/services/equipment.service';
 
 @Component({
-  selector: 'app-equipments',
-  templateUrl: './equipments.component.html',
-  styleUrls: ['./equipments.component.css']
+	selector: 'app-equipments',
+	templateUrl: './equipments.component.html',
+	styleUrls: [ './equipments.component.css' ]
 })
-export class EquipmentsComponent implements OnInit {
+export class EquipmentsComponent implements OnInit, AfterViewInit {
+	equipments: Equipment[];
+	dataSource: MatTableDataSource<Equipment> = new MatTableDataSource<Equipment>(this.equipments);
+	displayedColumns: string[] = [ 'code', 'name', 'maintenanceDate', 'amount', 'price' ];
+	@ViewChild(MatPaginator, { static: true })
+	paginator: MatPaginator;
+	@ViewChild(MatSort) sort: MatSort;
 
-  constructor() { }
+	constructor(private equipmentService: EquipmentService) {}
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+		this.loadEquipments();
+	}
 
+	private loadEquipments(): void {
+		this.equipmentService.getEquipments().subscribe((equipments) => {
+			this.equipments = equipments;
+			this.dataSource.data = this.equipments;
+		});
+	}
+
+	ngAfterViewInit(): void {
+		this.dataSource.paginator = this.paginator;
+		this.dataSource.sort = this.sort;
+	}
 }
