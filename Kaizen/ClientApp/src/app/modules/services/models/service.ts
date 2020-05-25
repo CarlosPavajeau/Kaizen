d7b72b2d67847cb94d275198ae65eaafa570e6df
Carlos Pavajeau <cantte098@gmail.com>
2020-05-25 12:40:31 -0500

    FrontEnd/Services: Add new attributes to Service model (#6)

--- Kaizen/ClientApp/src/app/modules/services/models/service.ts
+++ Kaizen/ClientApp/src/app/modules/services/models/service.ts
@@ -1,4 +1,7 @@
 import { ServiceType } from '@modules/services/models/service-type';
+import { Product } from '@modules/inventory/products/models/product';
+import { Employee } from '@modules/employees/models/employee';
+import { Equipment } from '@modules/inventory/equipments/models/equipment';
 
 export interface Service {
 	code: string;
@@ -6,4 +9,12 @@ export interface Service {
 	cost: number;
 	serviceTypeId: number;
 	serviceType?: ServiceType;
+
+	productCodes: string[];
+	equipmentCodes: string[];
+	employeeCodes: string[];
+
+	products?: Product[];
+	equipments?: Equipment[];
+	employees?: Employee[];
 }
