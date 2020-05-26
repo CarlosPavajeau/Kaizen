41c0e4ef3d7f1ad54f809de4267e9e3a3a7ac656
Carlos Pavajeau <cantte098@gmail.com>
2020-05-26 09:34:46 -0500

    BackEnd/Infrastructure/Security: Implement ITokenGenerator

---
+++ Infrastructure/Security/TokenGenerator.cs
@@ -0,0 +1,41 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Kaizen.Core.Security;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Kaizen.Infrastructure.Security
+{
+    public class TokenGenerator : ITokenGenerator
+    {
+        public TokenGenerator(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public string GenerateToken(string username, string role)
+        {
+            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+            byte[] key = Encoding.ASCII.GetBytes(Configuration["AppSettings:Key"]);
+
+            SecurityTokenDescriptor tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, username),
+                    new Claim(ClaimTypes.Role, role)
+                }),
+                Expires = DateTime.Now.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            SecurityToken token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
