8e69453bb63afdbd5016674de68707c3ffa44c25
Carlos Pavajeau <cantte098@gmail.com>
2020-05-28 14:36:37 -0500

    BackEnd/Domain/Migrations: Add 'AddProductDescription' migration

---
+++ Domain/Migrations/20200528192447_AddProductDescription.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Kaizen.Domain.Migrations
+{
+    public partial class AddProductDescription : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Products",
+                maxLength: 350,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Products");
+        }
+    }
+}
