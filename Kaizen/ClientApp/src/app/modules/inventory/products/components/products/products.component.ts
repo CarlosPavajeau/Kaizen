3e379f3270283d3d7cd00445431b2c8970b020dc
Carlos Pavajeau <cantte098@gmail.com>
2020-05-28 15:10:41 -0500

    FrontEnd/Inventory/Products: Make product table (#4)

--- Kaizen/ClientApp/src/app/modules/inventory/products/components/products/products.component.ts
+++ Kaizen/ClientApp/src/app/modules/inventory/products/components/products/products.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { Product } from '../../models/product';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { ProductService } from '../../services/product.service';
 
 @Component({
-  selector: 'app-products',
-  templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+	selector: 'app-products',
+	templateUrl: './products.component.html',
+	styleUrls: [ './products.component.css' ]
 })
-export class ProductsComponent implements OnInit {
+export class ProductsComponent implements OnInit, AfterViewInit {
+	products: Product[];
+	dataSource: MatTableDataSource<Product> = new MatTableDataSource<Product>(this.products);
+	displayedColumns: string[] = [ 'code', 'name', 'amount', 'presentation', 'price' ];
+	@ViewChild(MatPaginator, { static: true })
+	paginator: MatPaginator;
+	@ViewChild(MatSort) sort: MatSort;
 
-  constructor() { }
+	constructor(private productService: ProductService) {}
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+		this.loadProducts();
+	}
 
+	private loadProducts(): void {
+		this.productService.getProducts().subscribe((products) => {
+			this.products = products;
+			this.dataSource.data = this.products;
+		});
+	}
+
+	ngAfterViewInit(): void {
+		this.dataSource.paginator = this.paginator;
+		this.dataSource.sort = this.sort;
+	}
 }
