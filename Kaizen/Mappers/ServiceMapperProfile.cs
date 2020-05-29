fb6375dee32e089f3e3787597ff6890a650715da
Carlos Pavajeau <cantte098@gmail.com>
2020-05-29 12:04:24 -0500

    BackEnd/Mappers/Service: Add AfterMap functionality in map ServiceInputModel to Service

--- Kaizen/Mappers/ServiceMapperProfile.cs
+++ Kaizen/Mappers/ServiceMapperProfile.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using AutoMapper;
 using Kaizen.Domain.Entities;
 using Kaizen.Models.Service;
@@ -9,7 +10,26 @@ namespace Kaizen.Mappers
         public ServiceMapperProfile()
         {
             CreateMap<ServiceEditModel, Service>();
-            CreateMap<ServiceInputModel, Service>();
+
+            CreateMap<ServiceInputModel, Service>().AfterMap((serviceModel, service) =>
+            {
+                service.EquipmentsServices = new List<EquipmentService>();
+                service.ProductsServices = new List<ProductService>();
+                service.EmployeesServices = new List<EmployeeService>();
+                foreach (var equipmentCode in serviceModel.EquipmentCodes)
+                {
+                    service.EquipmentsServices.Add(new EquipmentService { EquipmentCode = equipmentCode, Service = service });
+                }
+                foreach (var productCode in serviceModel.ProductCodes)
+                {
+                    service.ProductsServices.Add(new ProductService { ProductCode = productCode, Service = service });
+                }
+                foreach (var employeeCode in serviceModel.EmployeeCodes)
+                {
+                    service.EmployeesServices.Add(new EmployeeService { EmployeeId = employeeCode, Service = service });
+                }
+            });
+
             CreateMap<ServiceType, ServiceTypeViewModel>();
             CreateMap<Service, ServiceViewModel>();
         }
