145af754f5920937af5751fb08dc47171b6e5e1b
Carlos Pavajeau <cantte098@gmail.com>
2020-06-03 18:01:11 -0500

    FrontEnd/Clients: Change client model (#2)

--- Kaizen/ClientApp/src/app/modules/clients/models/client.ts
+++ Kaizen/ClientApp/src/app/modules/clients/models/client.ts
@@ -1,17 +1,20 @@
 import { ContactPerson } from './contact-person';
 import { ClientAddress } from './client-address';
 import { Person } from '@shared/models/person';
+import { ClientState } from './client-state';
 
 export interface Client extends Person {
 	clientType?: string;
-	NIT?: string;
+	nit?: string;
 	busninessName?: string;
 	tradeName?: string;
 	firstPhoneNumber: string;
 	secondPhoneNumber?: string;
-	firstLandline?: string;
-	secondLandline?: string;
+	firstLandLine?: string;
+	secondLandLine?: string;
 
 	contactPeople: ContactPerson[];
 	clientAddress: ClientAddress;
+
+	state: ClientState;
 }
