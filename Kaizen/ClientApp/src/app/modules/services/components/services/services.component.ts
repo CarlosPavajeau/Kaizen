8c4089c72c00c4e362d817e6b1860ee521e485b1
Carlos Pavajeau <cantte098@gmail.com>
2020-06-03 10:16:34 -0500

    FrontEnd/Services: Add services data table (#6)

--- Kaizen/ClientApp/src/app/modules/services/components/services/services.component.ts
+++ Kaizen/ClientApp/src/app/modules/services/components/services/services.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { Service } from '@modules/services/models/service';
+import { ServiceService } from '@modules/services/services/service.service';
 
 @Component({
-  selector: 'app-services',
-  templateUrl: './services.component.html',
-  styleUrls: ['./services.component.css']
+	selector: 'app-services',
+	templateUrl: './services.component.html',
+	styleUrls: [ './services.component.css' ]
 })
-export class ServicesComponent implements OnInit {
+export class ServicesComponent implements OnInit, AfterViewInit {
+	services: Service[];
+	dataSource: MatTableDataSource<Service> = new MatTableDataSource<Service>(this.services);
+	displayedColumns: string[] = [ 'code', 'name', 'type', 'cost' ];
+	@ViewChild(MatPaginator, { static: true })
+	paginator: MatPaginator;
+	@ViewChild(MatSort) sort: MatSort;
 
-  constructor() { }
+	constructor(private serviceService: ServiceService) {}
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+		this.loadServices();
+	}
 
+	private loadServices(): void {
+		this.serviceService.getServices().subscribe((services) => {
+			this.services = services;
+			this.dataSource.data = this.services;
+		});
+	}
+
+	ngAfterViewInit(): void {
+		this.dataSource.paginator = this.paginator;
+		this.dataSource.sort = this.sort;
+	}
 }
