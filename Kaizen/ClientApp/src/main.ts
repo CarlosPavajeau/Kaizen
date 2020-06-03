770e388b2621f0ec092acbabc45645ab5cb90b8f
Carlos Pavajeau <cantte098@gmail.com>
2020-06-03 08:15:53 -0500

    Format files

--- Kaizen/ClientApp/src/main.ts
+++ Kaizen/ClientApp/src/main.ts
@@ -1,20 +1,16 @@
+import { AppModule } from './app/app.module';
 import { enableProdMode } from '@angular/core';
+import { environment } from './environments/environment';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
-
 export function getBaseUrl() {
-  return document.getElementsByTagName('base')[0].href;
+	return document.getElementsByTagName('base')[0].href;
 }
 
-const providers = [
-  { provide: 'BASE_URL', useFactory: getBaseUrl, deps: [] }
-];
+const providers = [ { provide: 'BASE_URL', useFactory: getBaseUrl, deps: [] } ];
 
 if (environment.production) {
-  enableProdMode();
+	enableProdMode();
 }
 
-platformBrowserDynamic(providers).bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+platformBrowserDynamic(providers).bootstrapModule(AppModule).catch((err) => console.log(err));
