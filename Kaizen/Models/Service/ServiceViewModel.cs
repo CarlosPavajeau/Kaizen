3971ce5c34c2f06e8d2bfeaa07b9f30d0705ee0f
Carlos Pavajeau <cantte098@gmail.com>
2020-06-03 10:14:35 -0500

    BackEnd/Models/Service: Add new properties in ServiceViewModel

--- Kaizen/Models/Service/ServiceViewModel.cs
+++ Kaizen/Models/Service/ServiceViewModel.cs
@@ -1,6 +1,16 @@
+using System.Collections.Generic;
+using Kaizen.Models.Employee;
+using Kaizen.Models.Equipment;
+using Kaizen.Models.Product;
+
 namespace Kaizen.Models.Service
 {
     public class ServiceViewModel : ServiceInputModel
     {
+        public ServiceTypeViewModel ServiceType { get; set; }
+
+        public List<ProductViewModel> Products { get; set; }
+        public List<EquipmentViewModel> Equipments { get; set; }
+        public List<EmployeeViewModel> Employees { get; set; }
     }
 }
