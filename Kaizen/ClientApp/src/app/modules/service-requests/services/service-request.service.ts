6c6c9c61c93e66f9b5e30b2a03bae828e1db8700
Carlos Pavajeau <cantte098@gmail.com>
2020-06-04 17:14:43 -0500

    FrontEnd/ServiceRequests: Make service request service (#17)

--- Kaizen/ClientApp/src/app/modules/service-requests/services/service-request.service.ts
+++ Kaizen/ClientApp/src/app/modules/service-requests/services/service-request.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { SERVICE_REQUESTS_API_URL } from '@global/endpoints';
+import { ServiceRequest } from '@modules/service-requests/models/service-request';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class ServiceRequestService {
+	constructor(private http: HttpClient) {}
 
-  constructor() { }
+	getServiceRequests(): Observable<ServiceRequest[]> {
+		return this.http.get<ServiceRequest[]>(SERVICE_REQUESTS_API_URL);
+	}
+
+	getServiceRequest(id: number): Observable<ServiceRequest> {
+		return this.http.get<ServiceRequest>(`${SERVICE_REQUESTS_API_URL}/${id}`);
+	}
+
+	saveServiceRequest(serviceRequest: ServiceRequest): Observable<ServiceRequest> {
+		return this.http.post<ServiceRequest>(SERVICE_REQUESTS_API_URL, serviceRequest);
+	}
+
+	updateServiceRequest(serviceRequest: ServiceRequest): Observable<ServiceRequest> {
+		return this.http.put<ServiceRequest>(`${SERVICE_REQUESTS_API_URL}/${serviceRequest.id}`, serviceRequest);
+	}
 }
