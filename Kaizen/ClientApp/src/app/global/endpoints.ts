5b25659f526792305657a7aa098e3a07199f4b4d
Carlos Pavajeau <cantte098@gmail.com>
2020-06-04 17:12:19 -0500

    FrontEnd/Global: Add 'SERVICE_REQUESTS_API_URL' new endpoint

--- Kaizen/ClientApp/src/app/global/endpoints.ts
+++ Kaizen/ClientApp/src/app/global/endpoints.ts
@@ -2,6 +2,7 @@ export const AUTH_API_URL = 'api/Users';
 export const CLIENTS_API_URL = 'api/Clients';
 export const EMPLOYEES_API_URL = 'api/Employees';
 export const SERVICES_API_URL = 'api/Services';
+export const SERVICE_REQUESTS_API_URL = 'api/ServiceRequests';
 export const PRODUCTS_API_URL = 'api/Products';
 export const EQUIPMENTS_API_URL = 'api/Equipments';
 export const UPLOAD_DOWNLOAD_API_URL = 'api/UploadDownload';
