a57042671c4ff5fe3d2b90d238fb55c68fae22cb
Carlos Pavajeau <cantte098@gmail.com>
2020-06-06 15:15:21 -0500

    FrontEnd/ServiceRequests: Add Periodicity model and PERIODICITIES constants (#17)

--- Kaizen/ClientApp/src/app/modules/service-requests/models/periodicity-type.ts
+++ Kaizen/ClientApp/src/app/modules/service-requests/models/periodicity-type.ts
@@ -9,3 +9,20 @@ export enum PeriodicityType {
 	Annual,
 	Casual
 }
+
+export interface Periodicity {
+	name: string;
+	type: PeriodicityType;
+}
+
+export const PERIODICITIES: Periodicity[] = [
+	{ name: 'Quincenal', type: PeriodicityType.Biweekly },
+	{ name: 'Mensual', type: PeriodicityType.Monthly },
+	{ name: 'BiMensual', type: PeriodicityType.BiMonthly },
+	{ name: 'Trimestral', type: PeriodicityType.Trimester },
+	{ name: 'Cuatrimestral', type: PeriodicityType.Quarter },
+	{ name: 'Quimensual', type: PeriodicityType.Quinquemestre },
+	{ name: 'Semestral', type: PeriodicityType.Biannual },
+	{ name: 'Anual', type: PeriodicityType.Annual },
+	{ name: 'Casual', type: PeriodicityType.Casual }
+];
