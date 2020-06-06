4f1ce91cd04ab7af6f94bfaf4ffb786f9332cd55
Carlos Pavajeau <cantte098@gmail.com>
2020-06-06 15:17:50 -0500

    FrontEnd/ServiceRequests: Make an incomplete service request form (#17)

--- Kaizen/ClientApp/src/app/modules/service-requests/components/service-request-register/service-request-register.component.ts
+++ Kaizen/ClientApp/src/app/modules/service-requests/components/service-request-register/service-request-register.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, AbstractControl, Validators } from '@angular/forms';
+import { ServiceRequestService } from '@modules/service-requests/services/service-request.service';
+import { ServiceService } from '@modules/services/services/service.service';
+import { IForm } from '@app/core/models/form';
+import { Service } from '@app/modules/services/models/service';
+import { Periodicity, PERIODICITIES } from '../../models/periodicity-type';
 
 @Component({
-  selector: 'app-service-request-register',
-  templateUrl: './service-request-register.component.html',
-  styleUrls: ['./service-request-register.component.css']
+	selector: 'app-service-request-register',
+	templateUrl: './service-request-register.component.html',
+	styleUrls: [ './service-request-register.component.css' ]
 })
-export class ServiceRequestRegisterComponent implements OnInit {
+export class ServiceRequestRegisterComponent implements OnInit, IForm {
+	serviceRequestForm: FormGroup;
+	services: Service[];
+	periodicities: Periodicity[];
 
-  constructor() { }
+	get controls(): { [key: string]: AbstractControl } {
+		return this.serviceRequestForm.controls;
+	}
 
-  ngOnInit(): void {
-  }
+	constructor(
+		private serviceRequestService: ServiceRequestService,
+		private serviceService: ServiceService,
+		private formBuilder: FormBuilder
+	) {}
 
+	ngOnInit(): void {
+		this.loadData();
+		this.initForm();
+	}
+
+	private loadData(): void {
+		this.periodicities = PERIODICITIES;
+		this.serviceService.getServices().subscribe((services) => {
+			this.services = services;
+		});
+	}
+
+	initForm(): void {
+		this.serviceRequestForm = this.formBuilder.group({
+			date: [ '', [ Validators.required ] ],
+			serviceCodes: [ '', [ Validators.required ] ],
+			periodicity: [ '', Validators.required ]
+		});
+	}
 }
