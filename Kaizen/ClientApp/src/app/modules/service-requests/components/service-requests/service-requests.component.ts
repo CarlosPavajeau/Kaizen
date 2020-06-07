f73a1bfa6649f95d25ba9fb1cdf08a5dafaad476
Carlos Pavajeau <cantte098@gmail.com>
2020-06-07 11:30:44 -0500

    FrontEnd/ServiceRequests: Make an incomplete view of service requests (#17)

--- Kaizen/ClientApp/src/app/modules/service-requests/components/service-requests/service-requests.component.ts
+++ Kaizen/ClientApp/src/app/modules/service-requests/components/service-requests/service-requests.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { RequestState } from '@modules/service-requests/models/request-state';
+import { ServiceRequest } from '@modules/service-requests/models/service-request';
+import { ServiceRequestService } from '@modules/service-requests/services/service-request.service';
 
 @Component({
-  selector: 'app-service-requests',
-  templateUrl: './service-requests.component.html',
-  styleUrls: ['./service-requests.component.css']
+	selector: 'app-service-requests',
+	templateUrl: './service-requests.component.html',
+	styleUrls: [ './service-requests.component.css' ]
 })
 export class ServiceRequestsComponent implements OnInit {
+	serviceRequests: ServiceRequest[];
 
-  constructor() { }
+	constructor(private serviceRequestService: ServiceRequestService) {}
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+		this.loadServiceRequests();
+	}
 
+	private loadServiceRequests(): void {
+		this.serviceRequestService.getServiceRequests().subscribe((serviceRequests) => {
+			this.serviceRequests = serviceRequests;
+		});
+	}
+
+	rejectServiceRequest(serviceRequest: ServiceRequest): void {
+		serviceRequest.state = RequestState.Rejected;
+		this.serviceRequestService.updateServiceRequest(serviceRequest).subscribe((serviceRequestUpdate) => {
+			if (serviceRequestUpdate) {
+				this.serviceRequests = this.serviceRequests.filter((s) => s.code != serviceRequestUpdate.code);
+			}
+		});
+	}
 }
