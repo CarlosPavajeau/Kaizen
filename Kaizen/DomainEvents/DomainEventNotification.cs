8f4a5febc5193a384e1bf5e175e9c10342463998
Carlos Pavajeau <cantte098@gmail.com>
2020-06-08 11:41:13 -0500

    BackEnd/DomainEvents: Add 'DomainEventNotification' class (#18)

---
+++ Kaizen/DomainEvents/DomainEventNotification.cs
@@ -0,0 +1,15 @@
+using Kaizen.Core.Domain;
+using MediatR;
+
+namespace Kaizen.DomainEvents
+{
+    public class DomainEventNotification<TDomainEvent> : INotification where TDomainEvent : IDomainEvent
+    {
+        public TDomainEvent DomainEvent { get; }
+
+        public DomainEventNotification(TDomainEvent domainEvent)
+        {
+            DomainEvent = domainEvent;
+        }
+    }
+}
