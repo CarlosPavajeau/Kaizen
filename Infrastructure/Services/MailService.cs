e7c740017558e5d6bdec150790735463dd93fbde
Carlos Pavajeau <cantte098@gmail.com>
2020-06-08 12:42:32 -0500

    BackEnd/Infastructure/Services: Implement MailService

--- Infrastructure/Services/MailService.cs
+++ Infrastructure/Services/MailService.cs
@@ -1,14 +1,53 @@
-﻿using Kaizen.Core.Services;
-using System;
+using System.Net.Mail;
 using System.Threading.Tasks;
+using Kaizen.Core.Services;
+using Kaizen.Infrastructure.Services.Configuration;
+using Microsoft.Extensions.Options;
 
 namespace Kaizen.Infrastructure.Services
 {
     public class MailService : IMailService
     {
-        public Task SendEmailAsync(string email, string subject, string message)
+        private readonly MailSettings _mailSettings;
+        private readonly SmtpClient _client = new SmtpClient();
+
+        private MailMessage MailMessage { get; set; }
+
+        public MailService(IOptions<MailSettings> options)
         {
-            throw new NotImplementedException();
+            _mailSettings = options.Value;
+            ConfigSMTPClient();
         }
+
+        private void ConfigSMTPClient()
+        {
+            _client.Host = _mailSettings.Host;
+            _client.Port = _mailSettings.Port;
+            _client.EnableSsl = _mailSettings.EnableSsl;
+            _client.UseDefaultCredentials = _mailSettings.UseDefaultCredentials;
+            _client.Credentials = _mailSettings.Credential;
+        }
+
+        public async Task SendEmailAsync(string email, string subject, string message)
+        {
+            ConfigEmail(email, subject, message);
+            await _client.SendMailAsync(MailMessage);
+        }
+
+        private void ConfigEmail(string email, string subject, string message)
+        {
+            MailMessage = new MailMessage
+            {
+                From = new MailAddress(_mailSettings.Credential.UserName),
+                Subject = subject,
+                Body = message,
+                IsBodyHtml = false,
+                Priority = MailPriority.High
+            };
+
+            MailMessage.To.Add(email);
+        }
+
+
     }
 }
