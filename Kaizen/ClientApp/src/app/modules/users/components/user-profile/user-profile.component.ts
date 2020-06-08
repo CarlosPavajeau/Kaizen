1620fcce648d39b07878ed4f5322f12ac9b38124
Carlos Pavajeau <cantte098@gmail.com>
2020-06-08 07:25:55 -0500

    FrontEnd/Users: Add new functionality in user profile component

--- Kaizen/ClientApp/src/app/modules/users/components/user-profile/user-profile.component.ts
+++ Kaizen/ClientApp/src/app/modules/users/components/user-profile/user-profile.component.ts
@@ -1,15 +1,39 @@
+import { AuthenticationService } from '@core/authentication/authentication.service';
+import { CLIENT_ROLE } from '@global/roles';
+import { ClientService } from '@modules/clients/services/client.service';
 import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from '@modules/employees/services/employee.service';
+import { Person } from '@shared/models/person';
 
 @Component({
-  selector: 'app-user-profile',
-  templateUrl: './user-profile.component.html',
-  styleUrls: ['./user-profile.component.css']
+	selector: 'app-user-profile',
+	templateUrl: './user-profile.component.html',
+	styleUrls: [ './user-profile.component.css' ]
 })
 export class UserProfileComponent implements OnInit {
+	person: Person;
 
-  constructor() { }
+	constructor(
+		private authService: AuthenticationService,
+		private clientService: ClientService,
+		private employeeService: EmployeeService
+	) {}
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+		this.loadData();
+	}
 
+	private loadData(): void {
+		const userRole = this.authService.getUserRole();
+		const user_id = this.authService.getCurrentUser().id;
+		if (userRole == CLIENT_ROLE) {
+			this.clientService.getClient(user_id).subscribe((client) => {
+				this.person = client;
+			});
+		} else {
+			this.employeeService.getEmployee(user_id).subscribe((employee) => {
+				this.person = employee;
+			});
+		}
+	}
 }
