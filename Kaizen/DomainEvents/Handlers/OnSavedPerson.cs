498b6d342fd73d1f9ba6d96571e28c43676c5c50
Carlos Pavajeau <cantte098@gmail.com>
2020-06-10 17:25:58 -0500

    BackEnd/DomainEvents/Handlers: Add 'OnSavedServiceRequest' and modify 'OnSavedPerson'

--- Kaizen/DomainEvents/Handlers/OnSavedPerson.cs
+++ Kaizen/DomainEvents/Handlers/OnSavedPerson.cs
@@ -2,7 +2,9 @@ using System.Threading;
 using System.Threading.Tasks;
 using Kaizen.Core.Services;
 using Kaizen.Domain.Events;
+using Kaizen.Hubs;
 using MediatR;
+using Microsoft.AspNetCore.SignalR;
 
 namespace Kaizen.DomainEvents.Handlers
 {
@@ -11,10 +13,12 @@ namespace Kaizen.DomainEvents.Handlers
         public class Handler : INotificationHandler<DomainEventNotification<SavedPerson>>
         {
             public readonly IMailService _mailService;
+            private readonly IHubContext<ClientHub> _clientHub;
 
-            public Handler(IMailService mailService)
+            public Handler(IMailService mailService, IHubContext<ClientHub> clientHub)
             {
                 _mailService = mailService;
+                _clientHub = clientHub;
             }
 
             public async Task Handle(DomainEventNotification<SavedPerson> notification, CancellationToken cancellationToken)
@@ -22,6 +26,7 @@ namespace Kaizen.DomainEvents.Handlers
                 SavedPerson domainEvent = notification.DomainEvent;
 
                 await _mailService.SendEmailAsync(domainEvent.Email, "Cliente Registrado", $"Hola, {domainEvent.FullName} acabas de registrarte en nuestra empresa.");
+                await _clientHub.Clients.Groups("Administrator", "OfficeEmployee").SendAsync("NewPersonRequest");
             }
         }
     }
