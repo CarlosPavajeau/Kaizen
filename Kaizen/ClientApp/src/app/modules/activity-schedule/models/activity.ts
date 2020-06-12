ac5675914d017372ded87f5e76a20e2086dd826d
Carlos Pavajeau <cantte098@gmail.com>
2020-06-12 18:41:23 -0500

    FrontEnd/ActivitySchedule: Add Activity model (#7)

---
+++ Kaizen/ClientApp/src/app/modules/activity-schedule/models/activity.ts
@@ -0,0 +1,20 @@
+import { Client } from '@modules/clients/models/client';
+import { Employee } from '@modules/employees/models/employee';
+import { PeriodicityType } from '@modules/service-requests/models/periodicity-type';
+import { RequestState } from '@modules/service-requests/models/request-state';
+import { Service } from '@modules/services/models/service';
+
+export interface Activity {
+	code?: number;
+	date: Date;
+	state: RequestState;
+	clientId: string;
+	periodicity: PeriodicityType;
+
+	employeeCodes: string;
+	serviceCodes: string;
+
+	employees?: Employee[];
+	services?: Service[];
+	client?: Client;
+}
