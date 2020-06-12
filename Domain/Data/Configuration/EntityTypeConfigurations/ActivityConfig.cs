5cb0dbd0760f86d0fd828a22f5b3c72b1b3ee406
Carlos Pavajeau <cantte098@gmail.com>
2020-06-12 11:53:10 -0500

    BackEnd/Domain/Data/Configuration: Rename table 'Activity' to 'Activities'

--- Domain/Data/Configuration/EntityTypeConfigurations/ActivityConfig.cs
+++ Domain/Data/Configuration/EntityTypeConfigurations/ActivityConfig.cs
@@ -8,7 +8,7 @@ namespace Kaizen.Domain.Data.Configuration.EntityTypeConfigurations
     {
         public void Configure(EntityTypeBuilder<Activity> builder)
         {
-            builder.ToTable("Activity");
+            builder.ToTable("Activities");
             builder.Property(a => a.Periodicity).HasDefaultValue(PeriodicityType.Casual);
         }
     }
