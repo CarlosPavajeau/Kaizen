409ab10bb6038b7672773d91e74f6b5595d66d87
Carlos Pavajeau <cantte098@gmail.com>
2020-06-12 18:44:42 -0500

    BackEnd/Models/Activity: Add 'Code' property in ActivityViewModel

--- Kaizen/Models/Activity/ActivityViewModel.cs
+++ Kaizen/Models/Activity/ActivityViewModel.cs
@@ -7,6 +7,8 @@ namespace Kaizen.Models.Activity
 {
     public class ActivityViewModel : ActivityInputModel
     {
+        public int Code { get; set; }
+
         public List<EmployeeViewModel> Employees { get; set; }
         public List<ServiceViewModel> Services { get; set; }
         public ClientViewModel Client { get; set; }
