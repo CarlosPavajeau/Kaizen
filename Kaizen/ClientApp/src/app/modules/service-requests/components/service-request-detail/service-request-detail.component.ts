1cc1b87502425062a1a0e2cc1047277a6d255de5
Carlos Pavajeau <cantte098@gmail.com>
2020-06-13 12:18:02 -0500

    FrontEnd/ServiceRequests: Make a ServiceRequestDetail component (#7)

--- Kaizen/ClientApp/src/app/modules/service-requests/components/service-request-detail/service-request-detail.component.ts
+++ Kaizen/ClientApp/src/app/modules/service-requests/components/service-request-detail/service-request-detail.component.ts
@@ -1,15 +1,43 @@
+import { ActivatedRoute, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { Employee } from '@modules/employees/models/employee';
+import { EmployeeService } from '@modules/employees/services/employee.service';
+import { RequestState } from '@modules/service-requests/models/request-state';
+import { ServiceRequest } from '@modules/service-requests/models/service-request';
+import { ServiceRequestService } from '@modules/service-requests/services/service-request.service';
 
 @Component({
-  selector: 'app-service-request-detail',
-  templateUrl: './service-request-detail.component.html',
-  styleUrls: ['./service-request-detail.component.css']
+	selector: 'app-service-request-detail',
+	templateUrl: './service-request-detail.component.html',
+	styleUrls: [ './service-request-detail.component.css' ]
 })
 export class ServiceRequestDetailComponent implements OnInit {
+	serviceRequest: ServiceRequest;
+	techniciansAvailable: Employee[] = [];
 
-  constructor() { }
+	constructor(
+		private serviceRequestService: ServiceRequestService,
+		private employeeService: EmployeeService,
+		private activateRoute: ActivatedRoute,
+		private router: Router
+	) {}
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+		const code = +this.activateRoute.snapshot.paramMap.get('code');
+		this.serviceRequestService.getServiceRequest(code).subscribe((serviceRequest) => {
+			this.serviceRequest = serviceRequest;
+			this.employeeService.getTechniciansAvailable(serviceRequest.date).subscribe((techniciansAvailable) => {
+				this.techniciansAvailable = techniciansAvailable;
+			});
+		});
+	}
 
+	cancelServiceRequest(): void {
+		this.serviceRequest.state = RequestState.Rejected;
+		this.serviceRequestService.updateServiceRequest(this.serviceRequest).subscribe((serviceRequestUpdate) => {
+			if (serviceRequestUpdate) {
+				this.router.navigateByUrl('/service_requests');
+			}
+		});
+	}
 }
