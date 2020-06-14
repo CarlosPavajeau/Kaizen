756532efab38a0b00f4a36236f4933765832c57a
Carlos Pavajeau <cantte098@gmail.com>
2020-06-14 17:25:17 -0500

    FrontEnd/ActivitySchedule: Add module imports

--- Kaizen/ClientApp/src/app/modules/activity-schedule/activity-schedule.module.ts
+++ Kaizen/ClientApp/src/app/modules/activity-schedule/activity-schedule.module.ts
@@ -5,13 +5,11 @@ import { ActivityScheduleRoutingModule } from './activity-schedule-routing.modul
 import { ActivityRegisterComponent } from './components/activity-register/activity-register.component';
 import { ActivityScheduleComponent } from './components/activity-schedule/activity-schedule.component';
 import { ActivityDetailComponent } from './components/activity-detail/activity-detail.component';
-
+import { SharedModule } from '@app/shared/shared.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [ActivityRegisterComponent, ActivityScheduleComponent, ActivityDetailComponent],
-  imports: [
-    CommonModule,
-    ActivityScheduleRoutingModule
-  ]
+	declarations: [ ActivityRegisterComponent, ActivityScheduleComponent, ActivityDetailComponent ],
+	imports: [ SharedModule, ActivityScheduleRoutingModule, FormsModule, ReactiveFormsModule ]
 })
-export class ActivityScheduleModule { }
+export class ActivityScheduleModule {}
