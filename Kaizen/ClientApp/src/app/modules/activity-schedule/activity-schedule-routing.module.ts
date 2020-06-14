9d929e734262f549269bbbb2a3d42630202ecf23
Carlos Pavajeau <cantte098@gmail.com>
2020-06-14 10:28:44 -0500

    FrontEnd/ActivitySchedule: Add routes (#7)

--- Kaizen/ClientApp/src/app/modules/activity-schedule/activity-schedule-routing.module.ts
+++ Kaizen/ClientApp/src/app/modules/activity-schedule/activity-schedule-routing.module.ts
@@ -1,11 +1,22 @@
+import { ActivityDetailComponent } from './components/activity-detail/activity-detail.component';
+import { ActivityRegisterComponent } from './components/activity-register/activity-register.component';
+import { ActivityScheduleComponent } from './components/activity-schedule/activity-schedule.component';
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: '',
+		children: [
+			{ path: '', component: ActivityScheduleComponent },
+			{ path: 'register', component: ActivityRegisterComponent },
+			{ path: ':code', component: ActivityDetailComponent }
+		]
+	}
+];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+	imports: [ RouterModule.forChild(routes) ],
+	exports: [ RouterModule ]
 })
-export class ActivityScheduleRoutingModule { }
+export class ActivityScheduleRoutingModule {}
