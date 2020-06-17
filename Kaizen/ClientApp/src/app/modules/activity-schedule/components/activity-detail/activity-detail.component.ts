08722bcdde5fe9068deaa74784af09915e82b373
Carlos Pavajeau <cantte098@gmail.com>
2020-06-17 13:59:25 -0500

    FrontEnd/ActivitySchedule: Make activity detail component (#7)

--- Kaizen/ClientApp/src/app/modules/activity-schedule/components/activity-detail/activity-detail.component.ts
+++ Kaizen/ClientApp/src/app/modules/activity-schedule/components/activity-detail/activity-detail.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivityScheduleService } from '../../services/activity-schedule.service';
+import { Activity } from '../../models/activity';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-activity-detail',
-  templateUrl: './activity-detail.component.html',
-  styleUrls: ['./activity-detail.component.css']
+	selector: 'app-activity-detail',
+	templateUrl: './activity-detail.component.html',
+	styleUrls: [ './activity-detail.component.css' ]
 })
 export class ActivityDetailComponent implements OnInit {
+	activity: Activity;
 
-  constructor() { }
+	constructor(private activityService: ActivityScheduleService, private activatedRoute: ActivatedRoute) {}
 
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {
+		const code = +this.activatedRoute.snapshot.paramMap.get('code');
+		this.activityService.getActivity(code).subscribe((activity) => {
+			this.activity = activity;
+		});
+	}
 }
