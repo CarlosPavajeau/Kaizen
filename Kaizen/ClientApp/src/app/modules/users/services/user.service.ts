edbe10a51f9e7db5c7ddba03dfd8266a3190d088
Carlos Pavajeau <cantte098@gmail.com>
2020-06-18 15:33:04 -0500

    FrontEnd/Users: Add User service

---
+++ Kaizen/ClientApp/src/app/modules/users/services/user.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '@app/core/models/user';
+import { AUTH_API_URL } from '@app/global/endpoints';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class UserService {
+	constructor(private http: HttpClient) {}
+
+	confirmEmail(token: string, email: string): Observable<User> {
+		return this.http.post<User>(`${AUTH_API_URL}/ConfirmEmail?token=${token}&email=${email}`, { token, email });
+	}
+}
