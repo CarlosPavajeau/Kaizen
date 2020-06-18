7ac4f70a07a521f98ccf75f72001b4fdfb1a360e
Carlos Pavajeau <cantte098@gmail.com>
2020-06-18 15:29:20 -0500

    BackEnd/DomainEvents/Handlers/OnSavedPerson: Rewrite functionality

--- Kaizen/DomainEvents/Handlers/OnSavedPerson.cs
+++ Kaizen/DomainEvents/Handlers/OnSavedPerson.cs
@@ -1,8 +1,10 @@
 using System.Threading;
 using System.Threading.Tasks;
 using Kaizen.Core.Services;
+using Kaizen.Domain.Entities;
 using Kaizen.Domain.Events;
 using Kaizen.Hubs;
+using Kaizen.Infrastructure.Services.MailTemplates;
 using MediatR;
 using Microsoft.AspNetCore.SignalR;
 
@@ -14,18 +16,25 @@ namespace Kaizen.DomainEvents.Handlers
         {
             public readonly IMailService _mailService;
             private readonly IHubContext<ClientHub> _clientHub;
+            private readonly IMailTemplate<ClientMailTemplate> _mailTemplate;
 
-            public Handler(IMailService mailService, IHubContext<ClientHub> clientHub)
+            public Handler(IMailService mailService, IHubContext<ClientHub> clientHub, IMailTemplate<ClientMailTemplate> mailTemplate)
             {
                 _mailService = mailService;
                 _clientHub = clientHub;
+                _mailTemplate = mailTemplate;
             }
 
             public async Task Handle(DomainEventNotification<SavedPerson> notification, CancellationToken cancellationToken)
             {
-                SavedPerson domainEvent = notification.DomainEvent;
+                Client savedPerson = notification.DomainEvent.Client;
+                string email = _mailTemplate.LoadTemplate($"{savedPerson.FirstName} {savedPerson.LastName}",
+                                                          $"{savedPerson.TradeName}", $"{savedPerson.ClientAddress.City}",
+                                                          $"{savedPerson.ClientAddress.Neighborhood}", $"{savedPerson.ClientAddress.Street}",
+                                                          $"{notification.DomainEvent.EmailConfirmationLink}");
 
-                await _mailService.SendEmailAsync(domainEvent.Email, "Cliente Registrado", $"Hola, {domainEvent.FullName} acabas de registrarte en nuestra empresa.");
+
+                await _mailService.SendEmailAsync(savedPerson.User.Email, "Cliente Registrado", email, true);
                 await _clientHub.Clients.Groups("Administrator", "OfficeEmployee").SendAsync("NewPersonRequest");
             }
         }
