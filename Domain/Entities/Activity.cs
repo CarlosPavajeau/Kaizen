287327e8cf226ec50903cc688a5a7c19a9d9f7bf
Carlos Pavajeau <cantte098@gmail.com>
2020-06-25 18:12:14 -0500

    BackEnd/Domain/Entities/Activity: Implement ICloneable interface (#9)

--- Domain/Entities/Activity.cs
+++ Domain/Entities/Activity.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
 
 namespace Kaizen.Domain.Entities
 {
-    public class Activity : RequestBase
+    public class Activity : RequestBase, ICloneable
     {
         public List<ActivityEmployee> ActivitiesEmployees { get; set; }
         public List<ActivityService> ActivitiesServices { get; set; }
@@ -12,5 +14,24 @@ namespace Kaizen.Domain.Entities
         public List<Employee> Employees { get; set; }
         [NotMapped]
         public List<Service> Services { get; set; }
+
+        public object Clone()
+        {
+            Activity activity = new Activity
+            {
+                Date = Date,
+                Client = Client,
+                ClientId = ClientId,
+                State = State,
+                Periodicity = Periodicity,
+                ActivitiesEmployees = new List<ActivityEmployee>(ActivitiesEmployees.Select(a => new ActivityEmployee { EmployeeId = a.EmployeeId })),
+                ActivitiesServices = new List<ActivityService>(ActivitiesServices.Select(a => new ActivityService { ServiceCode = a.ServiceCode }))
+            };
+
+            activity.ActivitiesEmployees.ForEach(a => a.Activity = activity);
+            activity.ActivitiesServices.ForEach(a => a.Activity = activity);
+
+            return activity;
+        }
     }
 }
