f02e6b47b182ae5acfa4c8d268ee0356ad98bb97
Carlos Pavajeau <cantte098@gmail.com>
2020-06-25 18:14:08 -0500

    BackEnd/DomainEvents/Handlers: Use 'ScheduleActivities' method

--- Kaizen/DomainEvents/Handlers/OnSavedActivity.cs
+++ Kaizen/DomainEvents/Handlers/OnSavedActivity.cs
@@ -4,6 +4,7 @@ using AutoMapper;
 using Kaizen.Core.Services;
 using Kaizen.Domain.Entities;
 using Kaizen.Domain.Events;
+using Kaizen.Domain.Repositories;
 using Kaizen.Hubs;
 using Kaizen.Models.Activity;
 using MediatR;
@@ -18,18 +19,20 @@ namespace Kaizen.DomainEvents.Handlers
             private readonly IHubContext<ActivityHub> _hubContext;
             private readonly IMapper _mapper;
             private readonly IMailService _mailService;
-            public Handler(IHubContext<ActivityHub> hubContext, IMapper mapper, IMailService mailService)
+            private readonly IActivitiesRepository _activitiesRepository;
+            public Handler(IHubContext<ActivityHub> hubContext, IMapper mapper, IMailService mailService, IActivitiesRepository activitiesRepository)
             {
                 _hubContext = hubContext;
                 _mapper = mapper;
                 _mailService = mailService;
+                _activitiesRepository = activitiesRepository;
             }
 
             public async Task Handle(DomainEventNotification<SavedActivity> notification, CancellationToken cancellationToken)
             {
                 ActivityViewModel activityModel = _mapper.Map<ActivityViewModel>(notification.DomainEvent.Activity);
                 await _hubContext.Clients.Groups("Clients").SendAsync("NewActivity", activityModel);
-
+                await _activitiesRepository.ScheduleActivities(notification.DomainEvent.Activity);
                 Client client = notification.DomainEvent.Activity.Client;
 
                 await _mailService.SendEmailAsync(client.User.Email, "Actividad pendiente", $"Estimado {client.LastName} {client.FirstName} hemos agendado " +
