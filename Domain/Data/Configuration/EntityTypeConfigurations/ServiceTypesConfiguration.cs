027a414dc997744685228127d1f5fcf3d54b5517
Carlos Pavajeau <cantte098@gmail.com>
2020-07-02 18:37:01 -0500

    BackEnd/Data/Configuration: Change ServiceTypes names

--- Domain/Data/Configuration/EntityTypeConfigurations/ServiceTypesConfiguration.cs
+++ Domain/Data/Configuration/EntityTypeConfigurations/ServiceTypesConfiguration.cs
@@ -11,11 +11,11 @@ namespace Kaizen.Domain.Data.Configuration.EntityTypeConfigurations
             builder.HasData(new ServiceType[]
             {
                 new ServiceType { Id = 1, Name = "Control de plagas" },
-                new ServiceType { Id = 2, Name = "Saneamiento" },
-                new ServiceType { Id = 3, Name = "Limpieza de espacios" },
-                new ServiceType { Id = 4, Name = "Lavado y desinfección de tanques de agua" },
-                new ServiceType { Id = 5, Name = "Captura y rehabilidación de animales domesticos" },
-                new ServiceType { Id = 6, Name = "Jardineria" }
+                new ServiceType { Id = 2, Name = "Desinfección de ambientes y superficies" },
+                new ServiceType { Id = 3, Name = "Captura y reubicación de animales" },
+                new ServiceType { Id = 4, Name = "Matenimiento de sistemas y equipos" },
+                new ServiceType { Id = 5, Name = "Jardinería" },
+                new ServiceType { Id = 6, Name = "Suministro, instalación y mantenimiento de equipos" }
             });
         }
     }
