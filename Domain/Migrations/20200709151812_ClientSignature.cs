d885a9408cc0b68257ab38ad4092cb9e4d47847c
Carlos Pavajeau <cantte098@gmail.com>
2020-07-09 10:20:37 -0500

    BackEnd/Domain/Migrations: Add ClientSignature migration

---
+++ Domain/Migrations/20200709151812_ClientSignature.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Kaizen.Domain.Migrations
+{
+    public partial class ClientSignature : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ClientSignature",
+                table: "WorkOrders",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ClientSignature",
+                table: "WorkOrders");
+        }
+    }
+}
