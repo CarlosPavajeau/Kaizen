e2983c158dce0dce0780af6ceb9865924aae65dd
Carlos Pavajeau <cantte098@gmail.com>
2020-07-10 18:11:24 -0500

    BackEnd/Domain/Migrations: Add UpdateStates migration

---
+++ Domain/Migrations/20200710225250_UpdateStates.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Kaizen.Domain.Migrations
+{
+    public partial class UpdateStates : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "State",
+                table: "Activities",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "State",
+                table: "Activities",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldDefaultValue: 0);
+        }
+    }
+}
