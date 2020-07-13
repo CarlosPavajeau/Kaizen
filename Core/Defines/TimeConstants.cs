af987693426d369673cf6e78da8f00c46e61122e
Carlos Pavajeau <cantte098@gmail.com>
2020-07-13 16:21:01 -0500

    Core/Defines: Add time constants

---
+++ Core/Defines/TimeConstants.cs
@@ -0,0 +1,9 @@
+namespace Kaizen.Core.Defines
+{
+    public static class TimeConstants
+    {
+        public const int Milliseconds = 1000;
+        public const int Seconds = 60;
+        public const int Minutes = 60;
+    }
+}
