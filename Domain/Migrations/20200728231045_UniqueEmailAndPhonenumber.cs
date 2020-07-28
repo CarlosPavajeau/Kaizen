81dc5c97f717952442b349c329f0545f293dd9a3
Carlos Pavajeau <cantte098@gmail.com>
2020-07-28 18:11:49 -0500

    BackEnd/Domain/Migrations: Add 'UniqueEmailAndPhonenumber' migration

---
+++ Domain/Migrations/20200728231045_UniqueEmailAndPhonenumber.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Kaizen.Domain.Migrations
+{
+    public partial class UniqueEmailAndPhonenumber : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_Email",
+                table: "AspNetUsers",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_PhoneNumber",
+                table: "AspNetUsers",
+                column: "PhoneNumber",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_Email",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_PhoneNumber",
+                table: "AspNetUsers");
+        }
+    }
+}
