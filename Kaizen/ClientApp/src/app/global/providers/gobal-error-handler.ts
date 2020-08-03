b8053e766794a3caca40ecfa700b285a53cf1308
Carlos Pavajeau <cantte098@gmail.com>
2020-08-03 11:33:19 -0500

    FrontEnd/Global: Rewrite GlobalErrorHandler

--- Kaizen/ClientApp/src/app/global/providers/gobal-error-handler.ts
+++ Kaizen/ClientApp/src/app/global/providers/gobal-error-handler.ts
@@ -1,26 +1,38 @@
 import { ErrorHandler, Injectable } from '@angular/core';
 import { HttpErrorResponse } from '@angular/common/http';
 import { NotificationsService } from '@shared/services/notifications.service';
+import { Router } from '@angular/router';
 
 @Injectable({
 	providedIn: 'root'
 })
 export class GlobalErrorHandler implements ErrorHandler {
-	constructor(private notificationsService: NotificationsService) {}
+	constructor(private notificationsService: NotificationsService, private router: Router) {}
 
-	handleError(error: Error | HttpErrorResponse): void {
+	handleError(error: HttpErrorResponse): void {
 		console.log(error);
-		if (error instanceof HttpErrorResponse) {
-			console.log(error.error.Message);
-			if (error.error.Message) {
-				this.notificationsService.addMessage(error.error.Message, 'Ok');
-			} else if (error.error.errors) {
-				for (const prop of Object.keys(error.error.errors)) {
-					error.error.errors[prop].forEach((element: string) => {
-						this.notificationsService.addMessage(element, 'Ok');
-					});
-				}
+		if (typeof error.error === 'string') {
+			this.notificationsService.showErrorMessage(error.error, () => {
+				this.redirectToProfile();
+			});
+		} else if (error.error.Message) {
+			this.notificationsService.showErrorMessage(error.error.Message, () => {
+				this.redirectToProfile();
+			});
+		} else if (error.error.errors) {
+			let errorMessage = 'Se presentaron los siguientes errores: \n';
+			for (const prop of Object.keys(error.error.errors)) {
+				error.error.errors[prop].forEach((element: string) => {
+					errorMessage += `, ${element}`;
+				});
 			}
+			this.notificationsService.showErrorMessage(errorMessage, () => {
+				this.redirectToProfile();
+			});
 		}
+	}
+
+	redirectToProfile(): void {
+		this.router.navigateByUrl('/user/profile');
 	}
 }
