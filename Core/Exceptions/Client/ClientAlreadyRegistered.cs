63ebad8bc73d05a06231dcb51817e95a07d6c56d
Carlos Pavajeau <cantte098@gmail.com>
2020-08-03 11:40:42 -0500

    BackEnd/Core/Exceptions: Add more expressive message in client exceptions

--- Core/Exceptions/Client/ClientAlreadyRegistered.cs
+++ Core/Exceptions/Client/ClientAlreadyRegistered.cs
@@ -2,12 +2,12 @@ namespace Kaizen.Core.Exceptions.Client
 {
     public class ClientAlreadyRegistered : HttpException
     {
-        public ClientAlreadyRegistered() : base(409, "El cliente ya esta registrado")
+        public ClientAlreadyRegistered() : base(409, "El cliente ya esta registrado.")
         {
 
         }
 
-        public ClientAlreadyRegistered(string clientCode) : base(409, $"La indentidicación {clientCode} ya se encuentra registrada")
+        public ClientAlreadyRegistered(string clientCode) : base(409, $"La indentidicación {clientCode} ya se encuentra registrada.")
         {
 
         }
--- Core/Exceptions/Client/ClientNotRegister.cs
+++ Core/Exceptions/Client/ClientNotRegister.cs
@@ -2,7 +2,7 @@ namespace Kaizen.Core.Exceptions.Client
 {
     public class ClientNotRegister : HttpException
     {
-        public ClientNotRegister() : base(500, "Error del servidor, cliente no registrado")
+        public ClientNotRegister() : base(500, "Error del servidor, el cliente no pudo ser registrado.")
         {
 
         }
