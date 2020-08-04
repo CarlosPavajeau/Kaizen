1f3c7bea9e1f179680f109fa6de16977772bfbfa
Carlos Pavajeau <cantte098@gmail.com>
2020-08-04 17:00:43 -0500

    BackEnd/Core: Remove unnecessary exceptions

--- Core/Exceptions/Equipments/EquipmentDoesNotExists.cs
+++
@@ -1,9 +0,0 @@
-namespace Kaizen.Core.Exceptions.Equipments
-{
-    public class EquipmentDoesNotExists : HttpException
-    {
-        public EquipmentDoesNotExists() : base(400, "El equipo no esta registrado")
-        {
-        }
-    }
-}
--- Core/Exceptions/Equipments/EquipmentNotRegister.cs
+++
@@ -1,9 +0,0 @@
-namespace Kaizen.Core.Exceptions.Equipments
-{
-    public class EquipmentNotRegister : HttpException
-    {
-        public EquipmentNotRegister() : base(500, "Equipo no registrado")
-        {
-        }
-    }
-}
