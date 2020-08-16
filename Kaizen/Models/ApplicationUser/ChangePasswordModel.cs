d261d0032707a6b4e7ae43869a92ac7e9a8ac771
Carlos Pavajeau <cantte098@gmail.com>
2020-08-16 15:40:56 -0500

    BackEnd/Models/ApplicationUser: Add ChangePasswordModel

---
+++ Kaizen/Models/ApplicationUser/ChangePasswordModel.cs
@@ -0,0 +1,8 @@
+namespace Kaizen.Models.ApplicationUser
+{
+    public class ChangePasswordModel
+    {
+        public string OldPassword { get; set; }
+        public string NewPassword { get; set; }
+    }
+}
