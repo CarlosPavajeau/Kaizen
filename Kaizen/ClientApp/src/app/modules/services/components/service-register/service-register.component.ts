9bf6fd7966808c060c6a3742358ca82073e8c445
Cantte <cantte098@gmail.com>
2020-08-17 18:32:05 -0500

    FrontEnd/Services: Use Select components

--- Kaizen/ClientApp/src/app/modules/services/components/service-register/service-register.component.ts
+++ Kaizen/ClientApp/src/app/modules/services/components/service-register/service-register.component.ts
@@ -1,19 +1,14 @@
-import { Component, OnInit, QueryList, ViewChildren } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { MatSelectionList, MatSelectionListChange } from '@angular/material/list';
 import { Router } from '@angular/router';
 import { IForm } from '@core/models/form';
-import { Employee } from '@modules/employees/models/employee';
-import { EmployeeService } from '@modules/employees/services/employee.service';
-import { Equipment } from '@modules/inventory/equipments/models/equipment';
-import { EquipmentService } from '@modules/inventory/equipments/services/equipment.service';
-import { Product } from '@modules/inventory/products/models/product';
-import { ProductService } from '@modules/inventory/products/services/product.service';
 import { Service } from '@modules/services/models/service';
 import { ServiceType } from '@modules/services/models/service-type';
 import { ServiceService } from '@modules/services/services/service.service';
 import { NotificationsService } from '@shared/services/notifications.service';
-import { delay } from 'rxjs/operators';
+import { SelectEmployeesComponent } from '../select-employees/select-employees.component';
+import { SelectEquipmentsComponent } from '../select-equipments/select-equipments.component';
+import { SelectProductsComponent } from '../select-products/select-products.component';
 
 @Component({
   selector: 'app-service-register',
@@ -21,40 +16,19 @@ import { delay } from 'rxjs/operators';
   styleUrls: [ './service-register.component.css' ]
 })
 export class ServiceRegisterComponent implements OnInit, IForm {
-  products: Product[] = [];
-  equipments: Equipment[] = [];
-  employees: Employee[] = [];
   serviceTypes: ServiceType[] = [];
 
-  selectedProducts: Product[] = [];
-  selectedEquipments: Equipment[] = [];
-  selectedEmployees: Employee[] = [];
-
   serviceForm: FormGroup;
-  serviceEquipmentsForm: FormGroup;
-  serviceProductsForm: FormGroup;
-  serviceEmployeesForm: FormGroup;
 
-  productCode = this.formBuilder.control(null);
-  equipmentCode = this.formBuilder.control(null);
-  employeeCode = this.formBuilder.control(null);
-
-  showSelectedProducts = this.formBuilder.control(false);
-  showSelectedEquipments = this.formBuilder.control(false);
-  showSelectedEmployees = this.formBuilder.control(false);
-
-  @ViewChildren('productsListSelection') productsListSelection: QueryList<MatSelectionList>;
-  @ViewChildren('employeeListSelection') employeeListSelection: QueryList<MatSelectionList>;
-  @ViewChildren('equipmentListSelection') equipmentListSelection: QueryList<MatSelectionList>;
+  @ViewChild('selectProducts') selectProducts: SelectProductsComponent;
+  @ViewChild('selectEmployees') selectEmployees: SelectEmployeesComponent;
+  @ViewChild('selectEquipments') selectEquipments: SelectEquipmentsComponent;
 
   public get controls(): { [key: string]: AbstractControl } {
     return this.serviceForm.controls;
   }
 
   constructor(
-    private productService: ProductService,
-    private equipmentService: EquipmentService,
-    private employeeService: EmployeeService,
     private serviceService: ServiceService,
     private formBuilder: FormBuilder,
     private notificationService: NotificationsService,
@@ -67,16 +41,6 @@ export class ServiceRegisterComponent implements OnInit, IForm {
   }
 
   private loadData() {
-    this.productService.getProducts().subscribe((products) => {
-      this.products = products;
-    });
-
-    this.equipmentService.getEquipments().subscribe((equipments) => {
-      this.equipments = equipments;
-    });
-    this.employeeService.getEmployees().subscribe((employees) => {
-      this.employees = employees.filter((em) => [ 6, 7, 8 ].includes(em.chargeId));
-    });
     this.serviceService.getServiceTypes().subscribe((serviceTypes) => {
       this.serviceTypes = serviceTypes;
     });
@@ -88,89 +52,6 @@ export class ServiceRegisterComponent implements OnInit, IForm {
       name: [ '', [ Validators.required ] ],
       serviceType: [ '', [ Validators.required ] ],
       cost: [ '', [ Validators.required ] ]
-    });
-
-    this.initServiceEquipmentsForm();
-    this.initServiceEmployeeForm();
-    this.initServiceProductsForm();
-  }
-
-  private initServiceEquipmentsForm(): void {
-    this.serviceEquipmentsForm = this.formBuilder.group({
-      equipmentCodes: [ '', [ Validators.required ] ],
-      showSelectedEquipments: [ false ]
-    });
-
-    this.equipmentCode.valueChanges.pipe(delay(100)).subscribe((value) => {
-      if (this.selectedEquipments.length === 0 || this.equipmentListSelection.first.options === undefined) {
-        return;
-      }
-
-      const selectedOptions = this.equipmentListSelection.first.options.filter((option) => {
-        return this.selectedEquipments.indexOf(option.value) !== -1;
-      });
-
-      this.equipmentListSelection.first.selectedOptions.select(...selectedOptions);
-    });
-
-    this.showSelectedEquipments.valueChanges.subscribe((value) => {
-      if (value) {
-        this.equipmentCode.disable();
-      } else {
-        this.equipmentCode.enable();
-      }
-    });
-  }
-
-  private initServiceEmployeeForm(): void {
-    this.serviceEmployeesForm = this.formBuilder.group({
-      employeeCodes: [ '', [ Validators.required ] ]
-    });
-
-    this.employeeCode.valueChanges.pipe(delay(100)).subscribe((value) => {
-      if (this.selectedEmployees.length === 0 || this.employeeListSelection.first.options === undefined) {
-        return;
-      }
-
-      const selectedOptions = this.employeeListSelection.first.options.filter((option) => {
-        return this.selectedEmployees.indexOf(option.value) !== -1;
-      });
-
-      this.employeeListSelection.first.selectedOptions.select(...selectedOptions);
-    });
-
-    this.showSelectedEmployees.valueChanges.subscribe((value) => {
-      if (value) {
-        this.employeeCode.disable();
-      } else {
-        this.employeeCode.enable();
-      }
-    });
-  }
-
-  private initServiceProductsForm(): void {
-    this.serviceProductsForm = this.formBuilder.group({
-      productCodes: [ '', [ Validators.required ] ]
-    });
-
-    this.productCode.valueChanges.pipe(delay(100)).subscribe((value) => {
-      if (this.selectedProducts.length === 0 || this.productsListSelection.first.options === undefined) {
-        return;
-      }
-
-      const selectedOptions = this.productsListSelection.first.options.filter((option) => {
-        return this.selectedProducts.indexOf(option.value) !== -1;
-      });
-
-      this.productsListSelection.first.selectedOptions.select(...selectedOptions);
-    });
-
-    this.showSelectedProducts.valueChanges.subscribe((value) => {
-      if (value) {
-        this.productCode.disable();
-      } else {
-        this.productCode.enable();
-      }
     });
   }
 
@@ -190,10 +71,7 @@ export class ServiceRegisterComponent implements OnInit, IForm {
 
   private allFormsValid(): boolean {
     return (
-      this.serviceForm.valid &&
-      this.serviceProductsForm.valid &&
-      this.serviceEquipmentsForm.valid &&
-      this.serviceEmployeesForm.valid
+      this.serviceForm.valid && this.selectProducts.valid && this.selectEquipments.valid && this.selectEmployees.valid
     );
   }
 
@@ -203,33 +81,9 @@ export class ServiceRegisterComponent implements OnInit, IForm {
       name: this.controls['name'].value,
       serviceTypeId: +this.controls['serviceType'].value,
       cost: +this.controls['cost'].value,
-      productCodes: this.serviceProductsForm.controls['productCodes'].value.map((p: Product) => p.code),
-      equipmentCodes: this.serviceEquipmentsForm.controls['equipmentCodes'].value.map((e: Equipment) => e.code),
-      employeeCodes: this.serviceEmployeesForm.controls['employeeCodes'].value.map((e: Employee) => e.id)
+      productCodes: this.selectProducts.value,
+      equipmentCodes: this.selectEquipments.value,
+      employeeCodes: this.selectEmployees.value
     };
-  }
-
-  onSelectEmployee(event: MatSelectionListChange): void {
-    this.changeSelectedData<Employee>(this.selectedEmployees, event.option.value, event.option.selected);
-  }
-
-  onSelectProduct(event: MatSelectionListChange): void {
-    this.changeSelectedData<Product>(this.selectedProducts, event.option.value, event.option.selected);
-  }
-
-  onSelectEquipment(event: MatSelectionListChange): void {
-    this.changeSelectedData<Equipment>(this.selectedEquipments, event.option.value, event.option.selected);
-  }
-
-  private changeSelectedData<T>(data: T[], value: T, add_data: boolean): void {
-    if (add_data) {
-      data.push(value);
-    } else {
-      // Delete data
-      const index = data.indexOf(value);
-      if (index !== -1) {
-        data.splice(index, 1);
-      }
-    }
   }
 }
