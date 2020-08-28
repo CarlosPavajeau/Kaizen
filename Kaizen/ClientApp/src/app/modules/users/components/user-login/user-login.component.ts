1246d44ef8de8c75f7fe776e14efd7844cfbe876
Carlos Pavajeau <cantte098@gmail.com>
2020-08-28 09:20:44 -0500

    FrontEnd/Users: Change UserLoginComponent UI

--- Kaizen/ClientApp/src/app/modules/users/components/user-login/user-login.component.ts
+++ Kaizen/ClientApp/src/app/modules/users/components/user-login/user-login.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { HttpErrorHandlerService } from '@shared/services/http-error-handler.service';
 import { AuthenticationService } from '@core/authentication/authentication.service';
 import { IForm } from '@core/models/form';
 import { LoginRequest } from '@core/models/login-request';
+import { HttpErrorHandlerService } from '@shared/services/http-error-handler.service';
 import { of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
@@ -33,7 +33,7 @@ export class UserLoginComponent implements OnInit, IForm {
 
   initForm(): void {
     this.loginForm = this.formBuilder.group({
-      usernameOrEmail: [ '', [ Validators.required, Validators.minLength(5), Validators.maxLength(30) ] ],
+      usernameOrEmail: [ '', [ Validators.required, Validators.minLength(5), Validators.maxLength(15) ] ],
       password: [ '', [ Validators.required, Validators.minLength(8), Validators.maxLength(30) ] ]
     });
   }
