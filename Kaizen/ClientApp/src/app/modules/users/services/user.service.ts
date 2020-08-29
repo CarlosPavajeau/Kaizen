a448cf59b302ea483f8c8cbaf85a2bd0c8b71b31
Cantte <cantte098@gmail.com>
2020-08-29 12:55:43 -0500

    FrontEnd/Users: Add 'forgottenPassword' and 'resetPassword' methods in User service

--- Kaizen/ClientApp/src/app/modules/users/services/user.service.ts
+++ Kaizen/ClientApp/src/app/modules/users/services/user.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { User } from '@core/models/user';
 import { AUTH_API_URL } from '@global/endpoints';
 import { ChangePasswordModel } from '@modules/users/models/change-password';
+import { ResetPasswordModel } from '@modules/users/models/reset-password';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -17,5 +18,16 @@ export class UserService {
 
   changePassword(id: string, changePasswordModel: ChangePasswordModel): Observable<User> {
     return this.http.put<User>(`${AUTH_API_URL}/ChangePassword/${id}`, changePasswordModel);
+  }
+
+  forgottenPassword(usernameOrEmail: string): Observable<boolean> {
+    return this.http.post<boolean>(
+      `${AUTH_API_URL}/ForgottenPassword?usernameOrEmail=${usernameOrEmail}`,
+      usernameOrEmail
+    );
+  }
+
+  resetPassword(usernameOrEmail: string, resetPassword: ResetPasswordModel): Observable<User> {
+    return this.http.put<User>(`${AUTH_API_URL}/ResetPassword/${usernameOrEmail}`, resetPassword);
   }
 }
