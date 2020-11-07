fa0bd3483dac80c37f3f63aa210bb1846d0a282a
Carlos Pavajeau <cantte098@gmail.com>
2020-11-07 11:38:33 -0500

    feat(back-end): Add Payment model

---
+++ Kaizen/Models/Base/PaymentModel.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Kaizen.Models.Base
+{
+    public class PaymentModel
+    {
+        [Required]
+        public string Token { get; set; }
+        [Required]
+        public string PaymentMethodId { get; set; }
+    }
+}
