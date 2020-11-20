06338ff67eae66606e838d3b01de2733ba4348fa
Carlos Pavajeau <cantte098@gmail.com>
2020-11-20 14:37:28 -0500

    feat: Make PaymentMethod pipe
    #10

--- Kaizen/ClientApp/src/app/shared/pipes/payment-method.pipe.ts
+++ Kaizen/ClientApp/src/app/shared/pipes/payment-method.pipe.ts
@@ -1,10 +1,22 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { PaymentMethod } from '@base/app/modules/payments/models/payment-method';
 
 @Pipe({
   name: 'paymentMethod'
 })
 export class PaymentMethodPipe implements PipeTransform {
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: PaymentMethod): string {
+    switch (value) {
+      case PaymentMethod.None:
+        return 'Ninguno';
+      case PaymentMethod.Cash:
+        return 'Efectivo';
+      case PaymentMethod.CreditCard:
+        return 'Tarjeta de crédito';
+      case PaymentMethod.BankDeposit:
+        return 'Depósito bancario';
+      default:
+        return 'Método de pago inválido';
+    }
   }
 }
