d31557a6261924231fcfc74d64ff0b5d42402dd0
Carlos Pavajeau <cantte098@gmail.com>
2020-11-26 17:34:42 -0500

    feat(front-end): Implement loading functionality in ClientRequests component

--- Kaizen/ClientApp/src/app/modules/clients/components/client-requests/client-requests.component.ts
+++ Kaizen/ClientApp/src/app/modules/clients/components/client-requests/client-requests.component.ts
@@ -1,18 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Client } from '@modules/clients/models/client';
 import { ClientState } from '@modules/clients/models/client-state';
 import { ClientService } from '@modules/clients/services/client.service';
 import { NewClientSignalrService } from '@modules/clients/services/new-client-signalr.service';
+import { ObservableStatus } from '@shared/models/observable-with-status';
 import { NotificationsService } from '@shared/services/notifications.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-client-requests',
   templateUrl: './client-requests.component.html',
   styleUrls: [ './client-requests.component.scss' ]
 })
-export class ClientRequestsComponent implements OnInit {
+export class ClientRequestsComponent implements OnInit, OnDestroy {
+  public ObsStatus: typeof ObservableStatus = ObservableStatus;
+
   clientRequests: Client[];
-  updatingClientResquest = false;
+  clientRequests$: Observable<Client[]>;
+  updatingClientRequest = false;
 
   constructor(
     private clientService: ClientService,
@@ -29,27 +34,32 @@ export class ClientRequestsComponent implements OnInit {
     });
   }
 
+  ngOnDestroy(): void {
+    this.newClientSignalr.signalReceived.unsubscribe();
+  }
+
   private loadClientRequests(): void {
     this.clientService.getClientRequests().subscribe((clientRequests) => {
       this.clientRequests = clientRequests;
     });
+    this.clientRequests$ = this.clientService.getClientRequests();
   }
 
   acceptClient(client: Client): void {
     client.state = ClientState.Accepted;
-    this.proccessClient(client);
+    this.processClient(client);
   }
 
   rejectClient(client: Client): void {
     client.state = ClientState.Rejected;
-    this.proccessClient(client);
+    this.processClient(client);
   }
 
-  private proccessClient(client: Client): void {
-    this.updatingClientResquest = true;
+  private processClient(client: Client): void {
+    this.updatingClientRequest = true;
     this.clientService.updateClient(client).subscribe((clientUpdate) => {
       this.clientRequests = this.clientRequests.filter((c) => c.id !== clientUpdate.id);
-      this.updatingClientResquest = false;
+      this.updatingClientRequest = false;
     });
   }
 }
