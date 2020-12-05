8e6b942f1b6c38896486cc31f8f81731e66dd770
Carlos Pavajeau <cantte098@gmail.com>
2020-12-05 10:29:26 -0500

    fix(front-end): Show invoice payment button only to customers

--- Kaizen/ClientApp/src/app/modules/payments/components/service-invoice-detail/service-invoice-detail.component.ts
+++ Kaizen/ClientApp/src/app/modules/payments/components/service-invoice-detail/service-invoice-detail.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { AuthenticationService } from '@core/authentication/authentication.service';
+import { CLIENT_ROLE } from '@global/roles';
+import { InvoiceState } from '@modules/payments/models/invoice-state';
 import { ServiceInvoice } from '@modules/payments/models/service-invoice';
 import { ServiceInvoiceService } from '@modules/payments/services/service-invoice.service';
 import { ObservableStatus } from '@shared/models/observable-with-status';
@@ -12,10 +15,16 @@ import { Observable } from 'rxjs';
 })
 export class ServiceInvoiceDetailComponent implements OnInit {
   public ObsStatus: typeof ObservableStatus = ObservableStatus;
+  public InvoiceState: typeof InvoiceState = InvoiceState;
 
   serviceInvoice$: Observable<ServiceInvoice>;
+  isClient = false;
 
-  constructor(private serviceInvoiceService: ServiceInvoiceService, private activatedRoute: ActivatedRoute) {}
+  constructor(
+    private serviceInvoiceService: ServiceInvoiceService,
+    private activatedRoute: ActivatedRoute,
+    private authService: AuthenticationService
+  ) {}
 
   ngOnInit(): void {
     this.loadData();
@@ -24,5 +33,10 @@ export class ServiceInvoiceDetailComponent implements OnInit {
   private loadData(): void {
     const id = +this.activatedRoute.snapshot.paramMap.get('id');
     this.serviceInvoice$ = this.serviceInvoiceService.getServiceInvoice(id);
+
+    const role = this.authService.getUserRole();
+    if (role === CLIENT_ROLE) {
+      this.isClient = true;
+    }
   }
 }
