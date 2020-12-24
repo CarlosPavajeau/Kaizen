1c771d2bc5d1c09e600710013c6200e82ce52f11
Carlos Pavajeau <cantte098@gmail.com>
2020-12-24 11:43:28 -0500

    feat(front-end): Add animations in Header component

--- Kaizen/ClientApp/src/app/shared/components/header/header.component.ts
+++ Kaizen/ClientApp/src/app/shared/components/header/header.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { bounceInLeftOnEnterAnimation, bounceInOnEnterAnimation, bounceInUpOnEnterAnimation } from 'angular-animations';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: [ './header.component.scss' ]
+  styleUrls: [ './header.component.scss' ],
+  animations: [ bounceInLeftOnEnterAnimation(), bounceInOnEnterAnimation(), bounceInUpOnEnterAnimation() ]
 })
 export class HeaderComponent implements OnInit {
   constructor() {}
