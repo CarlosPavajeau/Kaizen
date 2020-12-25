1da057639f8bfa19c54ca1bcc3672ce20ba748d2
Carlos Pavajeau <cantte098@gmail.com>
2020-12-25 17:48:13 -0500

    test(back-end): Rewrite the method 'IAsyncQueryProvider.ExecuteAsync' to avoid errors in the execution of expressions

--- Kaizen.Test/Helpers/TestAsyncQueryProvider.cs
+++ Kaizen.Test/Helpers/TestAsyncQueryProvider.cs
@@ -38,7 +38,15 @@ namespace Kaizen.Test.Helpers
 
         TResult IAsyncQueryProvider.ExecuteAsync<TResult>(Expression expression, CancellationToken cancellationToken)
         {
-            return Execute<TResult>(expression);
+            var expectedResultType = typeof(TResult).GetGenericArguments()[0];
+            var executionResult = typeof(IQueryProvider)
+                .GetMethod(name: nameof(IQueryProvider.Execute), genericParameterCount: 1,
+                    types: new[] { typeof(Expression) })
+                .MakeGenericMethod(expectedResultType)
+                .Invoke(this, new[] { expression });
+
+            return (TResult)typeof(Task).GetMethod(nameof(Task.FromResult))?.MakeGenericMethod(expectedResultType)
+                .Invoke(null, new[] { executionResult });
         }
 
         public Task<object> ExecuteAsync(Expression expression, CancellationToken cancellationToken)
