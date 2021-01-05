ef09b7b8ae3ab301b453dc6f51e6cf32242cbe91
Carlos Pavajeau <cantte098@gmail.com>
2021-01-05 16:30:57 -0500

    refactor(front-end): Update Invoice and ProductInvoice models

--- Kaizen/ClientApp/src/app/modules/payments/models/product-invoice.ts
+++ Kaizen/ClientApp/src/app/modules/payments/models/product-invoice.ts
@@ -1,6 +1,7 @@
-import { ProductInvoiceDetail } from './product-invoice-detail';
 import { Invoice } from './invoice';
+import { ProductInvoiceDetail } from './product-invoice-detail';
 
 export interface ProductInvoice extends Invoice {
+  clientId: string;
   productInvoiceDetails: ProductInvoiceDetail[];
 }
