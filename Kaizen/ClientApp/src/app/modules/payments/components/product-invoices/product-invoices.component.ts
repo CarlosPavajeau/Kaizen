7c6b390062e9aa4b637bedd9fcb7fc095dd1575a
Carlos Pavajeau <cantte098@gmail.com>
2021-01-06 15:47:23 -0500

    feat(front-end): Make ProductInvoices component
    See also: #153

--- Kaizen/ClientApp/src/app/modules/payments/components/product-invoices/product-invoices.component.ts
+++ Kaizen/ClientApp/src/app/modules/payments/components/product-invoices/product-invoices.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { AuthenticationService } from '@core/authentication/authentication.service';
+import { CLIENT_ROLE } from '@global/roles';
+import { ProductInvoice } from '@modules/payments/models/product-invoice';
+import { ProductInvoiceService } from '@modules/payments/services/product-invoice.service';
+import { ObservableStatus } from '@shared/models/observable-with-status';
+import { Person } from '@shared/models/person';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-product-invoices',
@@ -6,7 +16,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: [ './product-invoices.component.scss' ]
 })
 export class ProductInvoicesComponent implements OnInit {
-  constructor() {}
+  public ObsStatus: typeof ObservableStatus = ObservableStatus;
 
-  ngOnInit(): void {}
+  productInvoices: ProductInvoice[];
+  productInvoices$: Observable<ProductInvoice[]>;
+
+  dataSource: MatTableDataSource<ProductInvoice> = new MatTableDataSource<ProductInvoice>([]);
+  displayedColumns: string[] = [ 'id', 'state', 'iva', 'subtotal', 'total', 'actions' ];
+
+  @ViewChild(MatPaginator, { static: true })
+  paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  isClient = false;
+
+  constructor(private productInvoiceService: ProductInvoiceService, private authService: AuthenticationService) {}
+
+  ngOnInit(): void {
+    this.loadData();
+  }
+
+  private loadData(): void {
+    const role = this.authService.getUserRole();
+    if (role === CLIENT_ROLE) {
+      this.isClient = true;
+      const currentPerson: Person = JSON.parse(localStorage.getItem('current_person'));
+      this.productInvoices$ = this.productInvoiceService.getClientProductInvoices(currentPerson.id);
+    } else {
+      this.productInvoices$ = this.productInvoiceService.getProductInvoices();
+    }
+
+    this.productInvoices$.subscribe((productInvoices) => {
+      this.dataSource.data = productInvoices;
+    });
+  }
 }
