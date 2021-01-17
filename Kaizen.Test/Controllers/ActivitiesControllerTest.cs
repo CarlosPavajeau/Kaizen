f1614e7634435cc5bb75bf1bc3b6f35c8e7149da
Carlos Pavajeau <cantte098@gmail.com>
2021-01-17 10:39:15 -0500

    test(back-end): Rewrite Activities controller tests
    See also: #119

--- Kaizen.Test/Controllers/ActivitiesControllerTest.cs
+++ Kaizen.Test/Controllers/ActivitiesControllerTest.cs
@@ -32,10 +32,13 @@ namespace Kaizen.Test.Controllers
 
             _activitiesController = new ActivitiesController(_activitiesRepository.Object, _clientsRepository.Object,
                 _unitWork.Object, ServiceProvider.GetService<IMapper>());
+
+            SetUpActivitiesRepository();
+            SetUpClientsRepository();
+            SetUpUnitWork();
         }
 
-        [Test]
-        public async Task GetActivities()
+        private void SetUpActivitiesRepository()
         {
             _activitiesRepository.Setup(r => r.GetAll()).Returns(new TestAsyncEnumerable<Activity>(new List<Activity>
             {
@@ -61,37 +64,14 @@ namespace Kaizen.Test.Controllers
                 }
             }).AsQueryable());
 
-            OkObjectResult result = (await _activitiesController.GetActivities()).Result as OkObjectResult;
-
-            Assert.NotNull(result);
-            Assert.NotNull(result.Value);
-
-            Assert.IsInstanceOf<IEnumerable<ActivityViewModel>>(result.Value);
-
-            IEnumerable<ActivityViewModel> value = result.Value as IEnumerable<ActivityViewModel>;
-            Assert.NotNull(value);
-            Assert.AreEqual(2, value.Count());
-        }
-
-        [Test]
-        public async Task GetActivity()
-        {
             _activitiesRepository.Setup(r => r.FindByIdAsync(1)).ReturnsAsync(new Activity
             {
                 Code = 1,
-                ClientId = "1007870922"
+                ClientId = "1007870922",
+                State = ActivityState.Pending
             });
+            _activitiesRepository.Setup(r => r.FindByIdAsync(3)).ReturnsAsync((Activity)null);
 
-            ActionResult<ActivityViewModel> result = await _activitiesController.GetActivity(1);
-
-            Assert.NotNull(result);
-            Assert.NotNull(result.Value);
-            Assert.AreEqual(1, result.Value.Code);
-        }
-
-        [Test]
-        public async Task EmployeeActivities()
-        {
             _activitiesRepository.Setup(r => r.GetPendingEmployeeActivities("32752423", It.IsAny<DateTime>()))
                 .ReturnsAsync(new[]
                 {
@@ -102,15 +82,6 @@ namespace Kaizen.Test.Controllers
                     }
                 });
 
-            OkObjectResult result = (await _activitiesController.EmployeeActivities("32752423", DateTime.Now)).Result as OkObjectResult;
-
-            Assert.NotNull(result);
-            Assert.NotNull(result.Value);
-        }
-
-        [Test]
-        public async Task ClientActivities()
-        {
             _activitiesRepository.Setup(r => r.GetPendingClientActivities("1007870922")).ReturnsAsync(new List<Activity>
             {
                 new()
@@ -120,15 +91,6 @@ namespace Kaizen.Test.Controllers
                 }
             });
 
-            OkObjectResult result = (await _activitiesController.ClientActivities("1007870922")).Result as OkObjectResult;
-
-            Assert.NotNull(result);
-            Assert.NotNull(result.Value);
-        }
-
-        [Test]
-        public async Task AppliedClientActivities()
-        {
             _activitiesRepository.Setup(r => r.GetAppliedClientActivities("1007870922")).ReturnsAsync(new List<Activity>
             {
                 new()
@@ -138,37 +100,11 @@ namespace Kaizen.Test.Controllers
                 }
             });
 
-            OkObjectResult result = (await _activitiesController.AppliedClientActivities("1007870922")).Result as OkObjectResult;
-
-            Assert.NotNull(result);
-            Assert.NotNull(result.Value);
-        }
-
-        [Test]
-        public async Task PutActivity()
-        {
-            _activitiesRepository.Setup(r => r.FindByIdAsync(1)).ReturnsAsync(new Activity
-            {
-                Code = 1,
-                ClientId = "1007870922",
-                State = ActivityState.Pending
-            });
+            _activitiesRepository.Setup(r => r.Insert(It.IsAny<Activity>())).Verifiable();
             _activitiesRepository.Setup(r => r.Update(It.IsAny<Activity>())).Verifiable();
-
-            _unitWork.Setup(r => r.SaveAsync()).Returns(Task.CompletedTask);
-
-            ActionResult<ActivityViewModel> result = await _activitiesController.PutActivity(1, new ActivityEditModel
-            {
-                State = ActivityState.Accepted,
-            });
-
-            Assert.NotNull(result);
-            Assert.NotNull(result.Value);
-            Assert.AreEqual(ActivityState.Accepted, result.Value.State);
         }
 
-        [Test]
-        public async Task PostActivity()
+        private void SetUpClientsRepository()
         {
             _clientsRepository.Setup(r => r.GetClientWithUser("1007870922")).ReturnsAsync(new Client
             {
@@ -180,9 +116,106 @@ namespace Kaizen.Test.Controllers
                     UserName = "client"
                 }
             });
-            _activitiesRepository.Setup(r => r.Insert(It.IsAny<Activity>())).Verifiable();
-            _unitWork.Setup(r => r.SaveAsync()).Returns(Task.CompletedTask);
 
+            _clientsRepository.Setup(r => r.GetClientWithUser("1007870930")).ReturnsAsync((Client)null);
+        }
+
+        private void SetUpUnitWork()
+        {
+            _unitWork.Setup(r => r.SaveAsync()).Returns(Task.CompletedTask);
+        }
+
+        [Test]
+        public async Task Get_All_Activities()
+        {
+            OkObjectResult result = (await _activitiesController.GetActivities()).Result as OkObjectResult;
+
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(result.Value);
+
+            Assert.IsInstanceOf<IEnumerable<ActivityViewModel>>(result.Value);
+
+            IEnumerable<ActivityViewModel> value = result.Value as IEnumerable<ActivityViewModel>;
+            Assert.IsNotNull(value);
+            Assert.AreEqual(2, value.Count());
+        }
+
+        [Test]
+        public async Task Get_Existing_Activity()
+        {
+            ActionResult<ActivityViewModel> result = await _activitiesController.GetActivity(1);
+
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(result.Value);
+            Assert.AreEqual(1, result.Value.Code);
+        }
+
+        [Test]
+        public async Task Get_Non_Existent_Activity()
+        {
+            ActionResult<ActivityViewModel> result = await _activitiesController.GetActivity(3);
+
+            Assert.IsNotNull(result);
+            Assert.IsNull(result.Value);
+            Assert.IsInstanceOf<NotFoundObjectResult>(result.Result);
+        }
+
+        [Test]
+        public async Task Get_Employee_Activities()
+        {
+            OkObjectResult result = (await _activitiesController.EmployeeActivities("32752423", DateTime.Now)).Result as OkObjectResult;
+
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(result.Value);
+        }
+
+        [Test]
+        public async Task Get_Client_Activities()
+        {
+            OkObjectResult result = (await _activitiesController.ClientActivities("1007870922")).Result as OkObjectResult;
+
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(result.Value);
+        }
+
+        [Test]
+        public async Task Get_Applied_Client_Activities()
+        {
+            OkObjectResult result = (await _activitiesController.AppliedClientActivities("1007870922")).Result as OkObjectResult;
+
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(result.Value);
+        }
+
+        [Test]
+        public async Task Update_Existing_Activity()
+        {
+            ActionResult<ActivityViewModel> result = await _activitiesController.PutActivity(1, new ActivityEditModel
+            {
+                State = ActivityState.Accepted,
+            });
+
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(result.Value);
+            Assert.AreEqual(ActivityState.Accepted, result.Value.State);
+        }
+
+        [Test]
+        public async Task Update_Non_Existent_Activity()
+        {
+            var result = await _activitiesController.PutActivity(3, new ActivityEditModel
+            {
+                State = ActivityState.Accepted,
+            });
+
+            Assert.IsNotNull(result);
+            Assert.IsNull(result.Value);
+            Assert.IsInstanceOf<BadRequestObjectResult>(result.Result);
+        }
+
+        [Test]
+        public async Task Save_Activity_With_Existing_Client()
+        {
             ActionResult<ActivityViewModel> result = await _activitiesController.PostActivity(new ActivityInputModel
             {
                 ClientId = "1007870922",
@@ -198,25 +231,43 @@ namespace Kaizen.Test.Controllers
                 }
             });
 
-            Assert.NotNull(result);
-            Assert.NotNull(result.Value);
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(result.Value);
+        }
+
+        [Test]
+        public async Task Save_Activity_With_Non_Existing_Client()
+        {
+            var result = await _activitiesController.PostActivity(new ActivityInputModel
+            {
+                ClientId = "1007870930",
+                Date = DateTime.Now,
+                ServiceCodes = new List<string>
+                {
+                    "123",
+                    "321"
+                },
+                EmployeeCodes = new List<string>
+                {
+                    "1007870945"
+                }
+            });
+
+            Assert.IsNotNull(result);
+            Assert.IsNull(result.Value);
+            Assert.IsInstanceOf<NotFoundObjectResult>(result.Result);
         }
 
         [Test]
         public async Task DeleteActivity()
         {
-            _activitiesRepository.Setup(r => r.FindByIdAsync(1)).ReturnsAsync(new Activity
-            {
-                Code = 1,
-                ClientId = "1007870922"
-            });
             _activitiesRepository.Setup(r => r.Delete(It.IsAny<Activity>())).Verifiable();
             _unitWork.Setup(r => r.SaveAsync()).Returns(Task.CompletedTask);
 
-            var result = await _activitiesController.DeleteActivity(1);
+            ActionResult<ActivityViewModel> result = await _activitiesController.DeleteActivity(1);
 
-            Assert.NotNull(result);
-            Assert.NotNull(result.Value);
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(result.Value);
         }
     }
 }
