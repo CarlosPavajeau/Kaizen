fbbbf8cbd495b105628b3b9ea7eebf92354c3fb5
Carlos Pavajeau <cantte098@gmail.com>
2021-01-17 16:18:34 -0500

    test(back-end): Add a rename test cases in ProductInvoices controller tests

--- Kaizen.Test/Controllers/ProductInvoicesControllerTest.cs
+++ Kaizen.Test/Controllers/ProductInvoicesControllerTest.cs
@@ -30,10 +30,12 @@ namespace Kaizen.Test.Controllers
 
             _productInvoicesController = new ProductInvoicesController(_productInvoicesRepository.Object,
                 _unitWork.Object, ServiceProvider.GetService<IMapper>());
+
+            SetUpProductInvoicesRepository();
+            SetUpUnitWork();
         }
 
-        [Test]
-        public async Task GetProductInvoices()
+        private void SetUpProductInvoicesRepository()
         {
             _productInvoicesRepository.Setup(r => r.GetAll()).Returns(new TestAsyncEnumerable<ProductInvoice>(
                 new List<ProductInvoice>
@@ -54,6 +56,28 @@ namespace Kaizen.Test.Controllers
                     }
                 }).AsQueryable());
 
+            _productInvoicesRepository.Setup(r => r.FindByIdAsync(1)).ReturnsAsync(new ProductInvoice
+            {
+                Id = 1,
+                ClientId = "1007870922",
+                GenerationDate = DateTime.Now,
+                ProductInvoiceDetails = new List<ProductInvoiceDetail>()
+            });
+            _productInvoicesRepository.Setup(r => r.FindByIdAsync(3)).ReturnsAsync((ProductInvoice)null);
+
+            _productInvoicesRepository.Setup(r => r.Update(It.IsAny<ProductInvoice>()));
+            _productInvoicesRepository.Setup(r => r.Insert(It.IsAny<ProductInvoice>())).Verifiable();
+
+        }
+
+        private void SetUpUnitWork()
+        {
+            _unitWork.Setup(r => r.SaveAsync()).Returns(Task.CompletedTask);
+        }
+
+        [Test]
+        public async Task Get_All_ProductInvoices()
+        {
             OkObjectResult result = (await _productInvoicesController.GetProductInvoices()).Result as OkObjectResult;
 
             Assert.NotNull(result);
@@ -62,16 +86,8 @@ namespace Kaizen.Test.Controllers
         }
 
         [Test]
-        public async Task GetProductInvoice()
+        public async Task Get_Existing_ProductInvoice()
         {
-            _productInvoicesRepository.Setup(r => r.FindByIdAsync(1)).ReturnsAsync(new ProductInvoice
-            {
-                Id = 1,
-                ClientId = "1007870922",
-                GenerationDate = DateTime.Now,
-                ProductInvoiceDetails = new List<ProductInvoiceDetail>()
-            });
-
             ActionResult<ProductInvoiceViewModel> result = await _productInvoicesController.GetProductInvoice(1);
 
             Assert.NotNull(result);
@@ -79,19 +95,18 @@ namespace Kaizen.Test.Controllers
         }
 
         [Test]
-        public async Task PutProductInvoice()
+        public async Task Get_Non_Existent_ProductInvoice()
         {
-            _productInvoicesRepository.Setup(r => r.FindByIdAsync(1)).ReturnsAsync(new ProductInvoice
-            {
-                Id = 1,
-                ClientId = "1007870922",
-                GenerationDate = DateTime.Now,
-                ProductInvoiceDetails = new List<ProductInvoiceDetail>()
-            });
-            _productInvoicesRepository.Setup(r => r.Update(It.IsAny<ProductInvoice>()));
-            _unitWork.Setup(r => r.SaveAsync()).Returns(Task.CompletedTask);
+            ActionResult<ProductInvoiceViewModel> result = await _productInvoicesController.GetProductInvoice(3);
 
+            Assert.IsNotNull(result);
+            Assert.IsNull(result.Value);
+            Assert.IsInstanceOf<NotFoundObjectResult>(result.Result);
+        }
 
+        [Test]
+        public async Task Update_Existing_ProductInvoice()
+        {
             ActionResult<ProductInvoiceViewModel> result = await _productInvoicesController.PutProductInvoice(1, new ProductInvoiceEditModel
             {
                 State = InvoiceState.Regenerated
@@ -102,12 +117,22 @@ namespace Kaizen.Test.Controllers
         }
 
         [Test]
-        public async Task PostProductInvoice()
+        public async Task Update_Non_Existent_ProductInvoice()
         {
-            _productInvoicesRepository.Setup(r => r.Insert(It.IsAny<ProductInvoice>())).Verifiable();
-            _unitWork.Setup(r => r.SaveAsync()).Returns(Task.CompletedTask);
+            ActionResult<ProductInvoiceViewModel> result = await _productInvoicesController.PutProductInvoice(3, new ProductInvoiceEditModel
+            {
+                State = InvoiceState.Regenerated
+            });
 
-            var result = await _productInvoicesController.PostProductInvoice(new ProductInvoiceInputModel
+            Assert.IsNotNull(result);
+            Assert.IsNull(result.Value);
+            Assert.IsInstanceOf<BadRequestObjectResult>(result.Result);
+        }
+
+        [Test]
+        public async Task Save_New_ProductInvoice()
+        {
+            ActionResult<ProductInvoiceViewModel> result = await _productInvoicesController.PostProductInvoice(new ProductInvoiceInputModel
             {
                 ClientId = "1007870922",
                 State = InvoiceState.Generated
