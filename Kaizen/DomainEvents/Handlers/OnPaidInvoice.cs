d02d85192d73f8a3d1785ed6fb8b7f2371f470d9
Carlos Pavajeau <cantte098@gmail.com>
2021-01-30 11:31:24 -0500

    feat(back-end): Notify the administrator when an invoice payment is made

--- Kaizen/DomainEvents/Handlers/OnPaidInvoice.cs
+++ Kaizen/DomainEvents/Handlers/OnPaidInvoice.cs
@@ -1,8 +1,14 @@
 using System.Threading;
 using System.Threading.Tasks;
+using AutoMapper;
+using Kaizen.Domain.Entities;
 using Kaizen.Domain.Events;
 using Kaizen.Domain.Repositories;
+using Kaizen.Hubs;
+using Kaizen.Models.ProductInvoice;
+using Kaizen.Models.ServiceInvoice;
 using MediatR;
+using Microsoft.AspNetCore.SignalR;
 
 namespace Kaizen.DomainEvents.Handlers
 {
@@ -11,15 +17,38 @@ namespace Kaizen.DomainEvents.Handlers
         public class Handler : INotificationHandler<DomainEventNotification<PaidInvoice>>
         {
             private readonly IStatisticsRepository _statisticsRepository;
+            private readonly IHubContext<InvoiceHub> _invoiceHub;
+            private readonly IMapper _mapper;
 
-            public Handler(IStatisticsRepository statisticsRepository)
+            public Handler(IStatisticsRepository statisticsRepository, IHubContext<InvoiceHub> invoiceHub,
+                IMapper mapper)
             {
                 _statisticsRepository = statisticsRepository;
+                _invoiceHub = invoiceHub;
+                _mapper = mapper;
             }
 
-            public async Task Handle(DomainEventNotification<PaidInvoice> notification, CancellationToken cancellationToken)
+            public async Task Handle(DomainEventNotification<PaidInvoice> notification,
+                CancellationToken cancellationToken)
             {
                 await _statisticsRepository.RegisterProfits(notification.DomainEvent.Invoice.Total);
+
+                switch (notification.DomainEvent.Invoice)
+                {
+                    case ProductInvoice productInvoice:
+                    {
+                        await _invoiceHub.Clients.Group("Administrator")
+                            .SendAsync("OnPaidProductInvoice", _mapper.Map<ProductInvoiceViewModel>(productInvoice),
+                                cancellationToken);
+                        break;
+                    }
+                    case ServiceInvoice serviceInvoice:
+                    {
+                        await _invoiceHub.Clients.Group("Administrator").SendAsync("OnPaidServiceInvoice",
+                            _mapper.Map<ServiceInvoiceViewModel>(serviceInvoice), cancellationToken);
+                        break;
+                    }
+                }
             }
         }
     }
