330499c85489858a06965b3b2e40a0ad37fa6550
Carlos Pavajeau <cantte098@gmail.com>
2021-01-31 18:09:48 -0500

    feat(front-end): Add InvoiceDetail component

---
+++ Kaizen/ClientApp/src/app/modules/payments/components/invoice-detail/invoice-detail.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Invoice } from "@modules/payments/models/invoice";
+
+@Component({
+  selector: 'app-invoice-detail',
+  templateUrl: './invoice-detail.component.html',
+  styleUrls: [ './invoice-detail.component.scss' ]
+})
+export class InvoiceDetailComponent implements OnInit {
+  @Input('invoice') invoice: Invoice;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+}
