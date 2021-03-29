7b61b35d025b5903c0742549e0ecf6b184ec9dbb
Cantte <cantte098@gmail.com>
2021-03-29 15:00:02 -0500

    feat(back-end): Add Notification url migration

---
+++ Domain/Migrations/20210329195346_NotificationUrl.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Kaizen.Domain.Migrations
+{
+    public partial class NotificationUrl : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Url",
+                table: "Notifications",
+                type: "longtext",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Url",
+                table: "Notifications");
+        }
+    }
+}
