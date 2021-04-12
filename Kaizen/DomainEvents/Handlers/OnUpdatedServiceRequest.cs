36a6adaf3fb730d33cbd62ebdf5fb5d4081aeca9
Cantte <cantte098@gmail.com>
2021-04-12 16:40:28 -0500

    feat(back-end): Send response email of a service request

--- Kaizen/DomainEvents/Handlers/OnUpdatedServiceRequest.cs
+++ Kaizen/DomainEvents/Handlers/OnUpdatedServiceRequest.cs
@@ -1,10 +1,13 @@
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 using AutoMapper;
+using Kaizen.Core.Services;
 using Kaizen.Domain.Entities;
 using Kaizen.Domain.Events;
 using Kaizen.Domain.Repositories;
 using Kaizen.Hubs;
+using Kaizen.Middleware;
 using Kaizen.Models.Notification;
 using MediatR;
 using Microsoft.AspNetCore.SignalR;
@@ -20,15 +23,20 @@ namespace Kaizen.DomainEvents.Handlers
             private readonly IClientsRepository _clientsRepository;
             private readonly INotificationsRepository _notificationsRepository;
             private readonly IUnitWork _unitWork;
+            private readonly IMailTemplate _mailTemplate;
+            private readonly IMailService _mailService;
 
             public Handler(IHubContext<NotificationHub> notificationsHub, IClientsRepository clientsRepository,
-                INotificationsRepository notificationsRepository, IUnitWork unitWork, IMapper mapper)
+                INotificationsRepository notificationsRepository, IUnitWork unitWork, IMapper mapper,
+                IMailTemplate mailTemplate, IMailService mailService)
             {
                 _notificationsHub = notificationsHub;
                 _clientsRepository = clientsRepository;
                 _notificationsRepository = notificationsRepository;
                 _unitWork = unitWork;
                 _mapper = mapper;
+                _mailTemplate = mailTemplate;
+                _mailService = mailService;
             }
 
             public async Task Handle(DomainEventNotification<UpdatedServiceRequest> notification,
@@ -50,6 +58,51 @@ namespace Kaizen.DomainEvents.Handlers
                 }
 
                 await SendServiceRequestNotification(clientNotification, client.User.Id, cancellationToken);
+                await SendServiceRequestResponseEmail(serviceRequest, client);
+            }
+
+            private async Task SendServiceRequestResponseEmail(ServiceRequest serviceRequest, Client client)
+            {
+                string responseMessage = serviceRequest.State switch
+                {
+                    ServiceRequestState.Accepted =>
+                        "Hemos aceptado tu solicitud de servicio y agendamos las actividades a aplicar.",
+                    ServiceRequestState.Rejected => "Desafortunadamente hemos rechazado tu solicitud de servicio.",
+                    ServiceRequestState.PendingSuggestedDate =>
+                        "Te hemos sugerido una nueva fecha de aplicación de nuestros servicios.",
+                    _ => "Tu solicitud aún está pendiente"
+                };
+
+                var responseUrl = new UriBuilder(KaizenHttpContext.BaseUrl)
+                {
+                    Path = serviceRequest.State switch
+                    {
+                        ServiceRequestState.Accepted => "/activity_schedule/client_schedule",
+                        ServiceRequestState.Rejected => "/service_requests/register",
+                        ServiceRequestState.PendingSuggestedDate => "/service_requests/new_date",
+                        _ => $"/service_requests/{serviceRequest.Code}"
+                    }
+                };
+
+                var responseButtonMessage = serviceRequest.State switch
+                {
+                    ServiceRequestState.Accepted => "Ver mi calendario de actividades",
+                    ServiceRequestState.Rejected => "Intentar hacer otra solicitud",
+                    ServiceRequestState.PendingSuggestedDate => "Ver fecha sugerida",
+                    _ => "Ver solicitud"
+                };
+
+                string emailMessage = _mailTemplate.LoadTemplate("ServiceRequestResponse.html",
+                    $"{client.FirstName} {client.LastName}",
+                    responseMessage,
+                    responseUrl.ToString(),
+                    responseButtonMessage);
+
+                await _mailService.SendEmailAsync(
+                    client.User.Email,
+                    "Respuesta de solicitud de servicio",
+                    emailMessage,
+                    true);
             }
 
             private async Task<Notification> SaveServiceRequestNotification(ServiceRequest serviceRequest,
