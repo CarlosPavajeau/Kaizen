6c7d6719362dd1c61495f64a0b302bb654d5a94f
Cantte <cantte098@gmail.com>
2021-04-16 19:14:37 -0500

    test(front-end): Fix Certificate service tests

--- Kaizen/ClientApp/src/app/modules/certificates/services/certificate.service.spec.ts
+++ Kaizen/ClientApp/src/app/modules/certificates/services/certificate.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { CertificateService } from './certificate.service';
@@ -6,7 +7,9 @@ describe('CertificateService', () => {
   let service: CertificateService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ]
+    });
     service = TestBed.inject(CertificateService);
   });
 
